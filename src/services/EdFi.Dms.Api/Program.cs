c1b2af817c310069dda4d6d18bf0a0e9c5570688
CSR2017 <suganya@imanisoftware.com>
2024-02-02 20:39:22 -0600

    Update title

--- src/services/EdFi.Dms.Api/Program.cs
+++ src/services/EdFi.Dms.Api/Program.cs
@@ -6,6 +6,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Data Management Service");
 
 app.Run();
