0d54696e97899c1d392758530c95107b28c2aa49
CSR2017 <suganya@imanisoftware.com>
2024-02-09 17:42:38 -0600

    Call temporary app settings log service

--- src/services/EdFi.DataManagementService.Api/Program.cs
+++ src/services/EdFi.DataManagementService.Api/Program.cs
@@ -3,7 +3,14 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.DataManagementService.Api.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddServices();
+
+builder.Services.AddLogging(builder => builder.AddConsole());
+builder.Services.AddSingleton<LogAppSettingsService>();
+
 var app = builder.Build();
 
 app.UsePathBase("/api");
@@ -11,6 +18,9 @@ app.UseRouting();
 
 app.MapGet("/", () => "Data Management Service");
 app.MapGet("/ping", () => Results.Ok(DateTime.Now));
+
+var appSettingsService = app.Services.GetRequiredService<LogAppSettingsService>();
+appSettingsService.Log();
 
 app.Run();
 
