ba373256d4d01df76f762d4c9bd1f8ad1524c267
CSR2017 <34072978+CSR2017@users.noreply.github.com>
2024-02-09 12:49:42 -0600

    [DMS-4]  Ping Endpoint (#3)
    * Add ping endpoint
    
    * Simple docker file to make sure app can be started on container
    
    * Fix gh pre-release step errors
    
    * Use chiseled aspnet image
    
    * Reduce severity for S1118
    
    * Add test for ping endpoint
    
    * Compliant solution for S1118

---
+++ src/services/EdFi.DataManagementService.Api.Tests/EndpointsTests.cs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System.Net;
+
+namespace EdFi.DataManagementService.Api.Tests
+{
+    [TestFixture]
+    public class EndpointsTests
+    {
+        [Test]
+        public async Task TestPingEndpoint()
+        {
+            // Arrange
+            await using var factory = new WebApplicationFactory<Program>();
+            using var client = factory.CreateClient();
+            var expectedDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+            // Act
+            var response = await client.GetAsync("/api/ping");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            content.Should().Contain(expectedDate);
+        }
+    }
+}
--- src/services/EdFi.DataManagementService.Api/Program.cs
+++ src/services/EdFi.DataManagementService.Api/Program.cs
@@ -6,6 +6,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UsePathBase("/api");
+app.UseRouting();
+
 app.MapGet("/", () => "Data Management Service");
+app.MapGet("/ping", () => Results.Ok(DateTime.Now));
 
 app.Run();
+
+public partial class Program
+{
+    // Compliant solution for Sonar lint S1118
+    private Program() { }
+}
