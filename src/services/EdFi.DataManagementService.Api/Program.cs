a7b9e6526633e2b8218015ff5c33953da8eca2e5
CSR2017 <suganya@imanisoftware.com>
2024-02-14 15:36:15 -0600

    Add LogAppSettings endpoint to do DI on options and logger

--- src/services/EdFi.DataManagementService.Api/Program.cs
+++ src/services/EdFi.DataManagementService.Api/Program.cs
@@ -3,7 +3,10 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.DataManagementService.Api.Configuration;
 using EdFi.DataManagementService.Api.Infrastructure;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServices();
@@ -11,14 +14,19 @@ builder.AddServices();
 var app = builder.Build();
 
 app.UsePathBase("/api");
+app.UseMiddleware<LoggingMiddleware>();
 app.UseRouting();
 app.UseRateLimiter();
 
-app.MapGet("/", () => "Data Management Service");
+app.MapGet("/", () => Results.Ok("Data Management Service"));
 app.MapGet("/ping", () => Results.Ok(DateTime.Now));
 
-var logAppSettingsService = app.Services.GetRequiredService<LogAppSettingsService>();
-logAppSettingsService.LogToConsole();
+app.MapGet("/LogAppSettings", (IOptions<AppSettings> options, ILogger<Program> logger) =>
+{
+    logger.LogDebug($"Log while debugging");
+    logger.LogInformation(message: JsonSerializer.Serialize(options.Value));
+    return Results.Ok("Successfully logged Application Settings");
+});
 
 app.Run();
 
