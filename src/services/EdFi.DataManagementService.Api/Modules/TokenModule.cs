87025da7904845d08d3f92ba1c9e979eb4d4155a
CSR2017 <suganya@imanisoftware.com>
2024-04-02 13:47:36 -0500

    Add request and response

--- src/services/EdFi.DataManagementService.Api/Modules/TokenModule.cs
+++ src/services/EdFi.DataManagementService.Api/Modules/TokenModule.cs
@@ -7,50 +7,79 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using EdFi.DataManagementService.Api.Configuration;
+using EdFi.DataManagementService.Api.Infrastructure.Extensions;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
-namespace EdFi.DataManagementService.Api.Modules
+namespace EdFi.DataManagementService.Api.Modules;
+
+public class TokenModule : IModule
 {
-    public class TokenModule : IModule
+    public void MapEndpoints(IEndpointRouteBuilder endpoints)
     {
-        public void MapEndpoints(IEndpointRouteBuilder endpoints)
+        endpoints.MapPost("/oauth/token", GenerateToken);
+    }
+
+    internal async Task GenerateToken(
+        HttpContext httpContext,
+        IOptions<AppSettings> options,
+        AuthenticationRequest authenticationRequest
+    )
+    {
+        if (
+            authenticationRequest == null
+            || (
+                string.IsNullOrEmpty(authenticationRequest.Key)
+                || string.IsNullOrEmpty(authenticationRequest.Secret)
+            )
+        )
         {
-            endpoints.MapGet("/oauth/token", GenerateToken);
+            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await httpContext.Response.WriteAsync("Please provide valid key and secret.");
+            return;
         }
 
-        internal async Task GenerateToken(HttpContext httpContext, IOptions<AppSettings> options)
+        var authSettings = options.Value.Authentication;
+
+        if (authSettings == null)
         {
-            var authSettings = options.Value.Authentication;
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsync("Authentication settings are missing.");
+        }
+        else
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authSettings.SigningKey));
 
-            if (authSettings == null)
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
             {
-                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await httpContext.Response.WriteAsync("Authentication settings are missing.");
-            }
-            else
-            {
-                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authSettings.SigningKey));
+                new Claim(ClaimTypes.NameIdentifier, "dms_client"),
+                new Claim(ClaimTypes.Role, "SIS Vendor")
+            };
 
-                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-                var claims = new[]
-                {
-                    new Claim(ClaimTypes.NameIdentifier, "dms_client"),
-                    new Claim(ClaimTypes.Role, "SIS Vendor")
-                };
-
-                var token = new JwtSecurityToken(
-                    authSettings.Issuer,
-                    claims: claims,
-                    expires: DateTime.Now.AddMinutes(60),
-                    signingCredentials: credentials
-                );
-
-                var generatedToken = new JwtSecurityTokenHandler().WriteToken(token);
-                httpContext.Response.StatusCode = StatusCodes.Status200OK;
-                await httpContext.Response.WriteAsync(generatedToken);
-            }
+            var token = new JwtSecurityToken(
+                authSettings.Issuer,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials
+            );
+            var generatedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            var tokenDetails = new TokenResponse { Token = generatedToken, ExpiresIn = "60 minutes" };
+            httpContext.Response.StatusCode = StatusCodes.Status200OK;
+            await httpContext.Response.WriteAsSerializedJsonAsync(tokenDetails);
         }
     }
+}
+
+public class AuthenticationRequest
+{
+    public required string Key { get; set; }
+    public required string Secret { get; set; }
+}
+
+public class TokenResponse
+{
+    public string? Token { get; set; }
+    public string? ExpiresIn { get; set; }
 }
