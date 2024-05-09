d629d87d0979d3b052e2caebb5a49b38561895f1
Adam Hopkins <adam.hopkins@simpat.tech>
2024-05-09 13:43:18 -0500

    Add database installer console application

---
+++ src/backend/EdFi.DataManagementService.Backend.Installer/Program.cs
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.DataManagementService.Backend.Installer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        switch (args[0])
+        {
+            case "postgresql":
+                new Postgresql.Deploy.DatabaseDeploy().DeployDatabase(args[1]);
+                break;
+            case "mssql":
+                new Mssql.Deploy.DatabaseDeploy().DeployDatabase(args[1]);
+                break;
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
