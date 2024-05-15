42f328b56a98b5b98d46244becac13c6695533b3
Adam Hopkins <adam.hopkins@simpat.tech>
2024-05-15 11:12:33 -0500

    Add npgsql

--- src/core/EdFi.DataManagementService.Core/Backend/PostgresqlDocumentStoreRepository.cs
+++ src/core/EdFi.DataManagementService.Core/Backend/PostgresqlDocumentStoreRepository.cs
@@ -3,27 +3,79 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.DataManagementService.Core.Model;
+using System.Text.Json.Nodes;
+using Dapper;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
 namespace EdFi.DataManagementService.Core.Backend;
 
-public class PostgresqlDocumentStoreRepository : IDocumentStoreRepository
+public class PostgresqlDocumentStoreRepository(ILogger<PostgresqlDocumentStoreRepository> _logger, string _connectionString = "host=localhost;port=5432;username=postgres;database=EdFi.DataManagementService;pooling=true;minimum pool size=10;maximum pool size=50;Application Name=EdFi.DataManagementService")
+    : IDocumentStoreRepository, IQueryHandler
 {
-    public Task<UpsertResult> UpsertDocument(UpsertRequest upsertRequest)
+    public async Task<UpsertResult> UpsertDocument(UpsertRequest upsertRequest)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation(_connectionString);
+        _logger.LogWarning(
+            "UpsertDocument(): Backend repository has been configured to always report success - {TraceId}",
+            upsertRequest.TraceId
+        );
+
+        using (var conn = new NpgsqlConnection(_connectionString))
+        {
+            conn.Open();
+            var command =
+                "INSERT INTO public.documents(id, document_partition_key, document_uuid, resource_name, edfi_doc) VALUES (1, 1, 'abc', 'test', 'fake');";
+            await conn.ExecuteAsync(command);
+        }
+        return await Task.FromResult<UpsertResult>(new UpsertResult.InsertSuccess());
     }
 
-    public Task<GetResult> GetDocumentById(GetRequest getRequest)
+    public async Task<GetResult> GetDocumentById(GetRequest getRequest)
     {
-        throw new NotImplementedException();
+        _logger.LogWarning(
+            "GetDocumentById(): Backend repository has been configured to always report success - {TraceId}",
+            getRequest.TraceId
+        );
+        return await Task.FromResult<GetResult>(
+            new GetResult.GetSuccess(
+                DocumentUuid: No.DocumentUuid,
+                EdfiDoc: new JsonObject(),
+                LastModifiedDate: DateTime.Now
+            )
+        );
     }
 
-    public Task<UpdateResult> UpdateDocumentById(UpdateRequest updateRequest)
+    public async Task<UpdateResult> UpdateDocumentById(UpdateRequest updateRequest)
     {
-        throw new NotImplementedException();
+        _logger.LogWarning(
+            "UpdateDocumentById(): Backend repository has been configured to always report success - {TraceId}",
+            updateRequest.TraceId
+        );
+        return await Task.FromResult<UpdateResult>(new UpdateResult.UpdateSuccess());
     }
 
-    public Task<DeleteResult> DeleteDocumentById(DeleteRequest deleteRequest)
+    public async Task<DeleteResult> DeleteDocumentById(DeleteRequest deleteRequest)
     {
-        throw new NotImplementedException();
+        _logger.LogWarning(
+            "DeleteDocumentById(): Backend repository has been configured to always report success  - {TraceId}",
+            deleteRequest.TraceId
+        );
+        return await Task.FromResult<DeleteResult>(new DeleteResult.DeleteSuccess());
+    }
+
+    public async Task<QueryResult> QueryDocuments(QueryRequest queryRequest)
+    {
+        _logger.LogWarning(
+            "QueryDocuments(): Backend repository has been configured to always report success - {TraceId}",
+            queryRequest.TraceId
+        );
+        return await Task.FromResult<QueryResult>(
+            new QueryResult.QuerySuccess(
+                TotalCount: 0,
+                EdfiDocs: []
+            )
+        );
     }
 }
