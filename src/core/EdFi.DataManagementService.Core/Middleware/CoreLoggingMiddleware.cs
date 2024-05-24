42a56ef23dac6c991712f4401b254fef4b695f4e
Johnny Brenes <johnny.brenes@simpat.tech>
2024-05-24 11:20:37 -0600

    remove unsed reference

--- src/core/EdFi.DataManagementService.Core/Middleware/CoreLoggingMiddleware.cs
+++ src/core/EdFi.DataManagementService.Core/Middleware/CoreLoggingMiddleware.cs
@@ -3,8 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Text.Encodings.Web;
-using System.Text.Json;
 using EdFi.DataManagementService.Core.Pipeline;
 using EdFi.DataManagementService.Core.Response;
 using Microsoft.Extensions.Logging;
