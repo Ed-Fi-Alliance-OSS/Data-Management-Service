a44f2387394f4ce7146bedbb106f391d6fed1442
CSR2017 <suganya@imanisoftware.com>
2024-05-28 17:51:05 -0500

    Add appsettings and application user

--- src/runapp.sh
+++ src/runapp.sh
@@ -7,14 +7,35 @@
 set -e
 set +x
 
+envsubst < /app/appsettings.template.json > /app/appsettings.json
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+
 until pg_isready -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U ${POSTGRES_USER}; do
   echo "Waiting for PostgreSQL to start..."
   sleep 2
 done
 
-echo "PostgreSQL is ready. Install Data Management Service schema."
+echo "PostgreSQL is ready."
 
-dotnet Installer/EdFi.DataManagementService.Backend.Installer.dll postgresql "host=${POSTGRES_HOST};port=${POSTGRES_PORT};username=${POSTGRES_USER};password=${POSTGRES_PASSWORD};database=edfi_datamanagementservice;"
+if [ "$NEED_DATABASE_SETUP" = true ]; then
+
+  DATABASE="edfi_datamanagementservice"
+
+  echo "Installing Data Management Service schema."
+  dotnet Installer/EdFi.DataManagementService.Backend.Installer.dll postgresql "host=${POSTGRES_HOST};port=${POSTGRES_PORT};username=${POSTGRES_USER};password=${POSTGRES_PASSWORD};database=${DATABASE};"
+
+  export NEED_DATABASE_SETUP=false
+
+  psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $DATABASE -c "CREATE USER $POSTGRES_APP_USER WITH PASSWORD '$POSTGRES_APP_PASSWORD';"
+  psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $DATABASE -c "GRANT ALL PRIVILEGES ON DATABASE $DATABASE TO $POSTGRES_APP_USER;"
+
+  psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $DATABASE -c "GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public to $POSTGRES_APP_USER;"
+
+
+else
+  echo "Skipping Data Management Service schema installation."
+fi
 
 dotnet EdFi.DataManagementService.Frontend.AspNetCore.dll
 
