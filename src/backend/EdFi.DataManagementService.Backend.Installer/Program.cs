ca08cb0a6971bdbb506e692b431823891b6d384f
Adam Hopkins <127156771+simpat-adam@users.noreply.github.com>
2024-06-03 14:14:10 -0500

    [DMS-195] Improve argument parsing and error handling of database installer (#111)

--- src/backend/EdFi.DataManagementService.Backend.Installer/Program.cs
+++ src/backend/EdFi.DataManagementService.Backend.Installer/Program.cs
@@ -3,23 +3,76 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Diagnostics;
+using CommandLine;
+using CommandLine.Text;
+using EdFi.DataManagementService.Backend.Deploy;
+
 namespace EdFi.DataManagementService.Backend.Installer;
+
+public class Options
+{
+    [Option('e', "engine", Required = true, HelpText = "The database engine to deploy. Valid values are 'postgresql' and 'mssql'")]
+    public string? DatabaseEngine { get; set; }
+
+    [Option('c', "connectionString", Required = true, HelpText = "The connection string to the database.")]
+    public string? ConnectionString { get; set; }
+}
 
 public static class Program
 {
+    static ParserResult<Options>? _parseResult;
     public static void Main(string[] args)
     {
-        switch (args[0])
+        var parser = new Parser(config => config.HelpWriter = null);
+        _parseResult = parser.ParseArguments<Options>(args);
+        _parseResult
+            .WithParsed(runOptions =>
+            {
+                switch (runOptions.DatabaseEngine)
+                {
+                    case "postgresql":
+                        HandleResult(new Postgresql.Deploy.DatabaseDeploy().DeployDatabase(runOptions.ConnectionString!));
+                        break;
+                    case "mssql":
+                        HandleResult(new Mssql.Deploy.DatabaseDeploy().DeployDatabase(runOptions.ConnectionString!));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid database engine specified.");
+                        DisplayHelp(_parseResult);
+                        break;
+                }
+            })
+            .WithNotParsed(_ =>
+            {
+                DisplayHelp(_parseResult);
+            });
+
+        if (Debugger.IsAttached)
         {
-            case "postgresql":
-                new Postgresql.Deploy.DatabaseDeploy().DeployDatabase(args[1]);
-                break;
-            case "mssql":
-                new Mssql.Deploy.DatabaseDeploy().DeployDatabase(args[1]);
-                break;
-            default:
-                throw new NotImplementedException();
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
         }
+    }
+
+    private static void HandleResult(DatabaseDeployResult result)
+    {
+        if (result is DatabaseDeployResult.DatabaseDeployFailure failure)
+        {
+            Console.WriteLine($"Failed to deploy database: {failure.Error.Message}");
+        }
+    }
+
+    private static void DisplayHelp<T>(ParserResult<T> parseResult)
+    {
+        var helpText = HelpText.AutoBuild(parseResult, h =>
+        {
+            h.Heading = "Ed-Fi Data Management Service Backend Installer";
+            h.Copyright = string.Empty;
+            h.AutoVersion = false;
+            return h;
+        }, e => e);
+        Console.WriteLine(helpText);
     }
 }
 
