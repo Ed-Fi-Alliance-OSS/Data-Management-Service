28abdecc9d86f1447361de08f290b3a9b3b4880b
CSR2017 <suganya@imanisoftware.com>
2024-06-06 18:11:56 -0500

    Remove else

--- src/backend/EdFi.DataManagementService.Backend.Postgresql/Operation/DeleteDocumentById.cs
+++ src/backend/EdFi.DataManagementService.Backend.Postgresql/Operation/DeleteDocumentById.cs
@@ -50,91 +50,89 @@ public class DeleteDocumentById(
                 await transaction.RollbackAsync();
                 return new DeleteResult.DeleteFailureNotExists();
             }
-            else
-            {
-                try
-                {
-                    rowsAffectedOnAliasTable = await _sqlAction.DeleteAliasByDocumentId(
-                        documentPartitionKey,
-                        document.Id,
-                        connection,
-                        transaction
-                    );
-                }
-                catch (PostgresException pe)
-                {
-                    return await HandlePostgresException("Aliases", pe);
-                }
 
-                if (rowsAffectedOnAliasTable == 0)
-                {
-                    _logger.LogCritical(
-                        "Failure: Associated Alias records are not available for the Document {DocumentId} - {TraceId}",
+            try
+            {
+                rowsAffectedOnAliasTable = await _sqlAction.DeleteAliasByDocumentId(
+                    documentPartitionKey,
+                    document.Id,
+                    connection,
+                    transaction
+                );
+            }
+            catch (PostgresException pe)
+            {
+                return await HandlePostgresException("Aliases", pe);
+            }
+
+            if (rowsAffectedOnAliasTable == 0)
+            {
+                _logger.LogCritical(
+                    "Failure: Associated Alias records are not available for the Document {DocumentId} - {TraceId}",
+                    deleteRequest.DocumentUuid,
+                    deleteRequest.TraceId
+                );
+            }
+
+            try
+            {
+                rowsAffected = await _sqlAction.DeleteDocumentByDocumentId(
+                    documentPartitionKey,
+                    document.Id,
+                    connection,
+                    transaction
+                );
+            }
+            catch (PostgresException pe)
+            {
+                return await HandlePostgresException("Documents", pe);
+            }
+
+            switch (rowsAffected)
+            {
+                case 1:
+                    await transaction.CommitAsync();
+                    return new DeleteResult.DeleteSuccess();
+                case 0:
+                    _logger.LogInformation(
+                        "Failure: Record to delete does not exist - {TraceId}",
+                        deleteRequest.TraceId
+                    );
+                    await transaction.RollbackAsync();
+                    return new DeleteResult.DeleteFailureNotExists();
+                default:
+                    _logger.LogError(
+                        "DeleteDocumentById rows affected was '{RowsAffected}' for {DocumentUuid} - {TraceId}",
+                        rowsAffected,
                         deleteRequest.DocumentUuid,
                         deleteRequest.TraceId
                     );
-                }
+                    await transaction.RollbackAsync();
+                    return new DeleteResult.UnknownFailure("Unknown Failure");
+            }
 
-                try
+            async Task<DeleteResult> HandlePostgresException(string tableName, PostgresException pe)
+            {
+                if (pe.SqlState == PostgresErrorCodes.SerializationFailure)
                 {
-                    rowsAffected = await _sqlAction.DeleteDocumentByDocumentId(
-                        documentPartitionKey,
-                        document.Id,
-                        connection,
-                        transaction
-                    );
-                }
-                catch (PostgresException pe)
-                {
-                    return await HandlePostgresException("Documents", pe);
-                }
-
-                switch (rowsAffected)
-                {
-                    case 1:
-                        await transaction.CommitAsync();
-                        return new DeleteResult.DeleteSuccess();
-                    case 0:
-                        _logger.LogInformation(
-                            "Failure: Record to delete does not exist - {TraceId}",
-                            deleteRequest.TraceId
-                        );
-                        await transaction.RollbackAsync();
-                        return new DeleteResult.DeleteFailureNotExists();
-                    default:
-                        _logger.LogError(
-                            "DeleteDocumentById rows affected was '{RowsAffected}' for {DocumentUuid} - {TraceId}",
-                            rowsAffected,
-                            deleteRequest.DocumentUuid,
-                            deleteRequest.TraceId
-                        );
-                        await transaction.RollbackAsync();
-                        return new DeleteResult.UnknownFailure("Unknown Failure");
-                }
-
-                async Task<DeleteResult> HandlePostgresException(string tableName, PostgresException pe)
-                {
-                    if (pe.SqlState == PostgresErrorCodes.SerializationFailure)
-                    {
-                        _logger.LogDebug(
-                            pe,
-                            "Transaction conflict on {TableName} table delete - {TraceId}",
-                            tableName,
-                            deleteRequest.TraceId
-                        );
-                        await transaction.RollbackAsync();
-                        return new DeleteResult.DeleteFailureWriteConflict();
-                    }
-
-                    _logger.LogError(
+                    _logger.LogDebug(
                         pe,
-                        "Failure on {TableName} table insert - {TraceId}",
+                        "Transaction conflict on {TableName} table delete - {TraceId}",
                         tableName,
                         deleteRequest.TraceId
                     );
                     await transaction.RollbackAsync();
-                    return new DeleteResult.UnknownFailure("Delete failure");
+                    return new DeleteResult.DeleteFailureWriteConflict();
                 }
+
+                _logger.LogError(
+                    pe,
+                    "Failure on {TableName} table insert - {TraceId}",
+                    tableName,
+                    deleteRequest.TraceId
+                );
+                await transaction.RollbackAsync();
+                return new DeleteResult.UnknownFailure("Delete failure");
             }
         }
         catch (Exception ex)
