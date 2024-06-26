ad2622822f6fb0b66ea622955cdff9db4c3baaf2
CSR2017 <suganya@imanisoftware.com>
2024-06-26 16:06:19 -0500

    Add sdk tests ps1 files

---
+++ eng/smoke_test/Invoke-NonDestructiveSdkTests.ps1
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+#Requires -Version 7
+
+param(
+  [string]
+  $Key = "sampleKey",
+
+  [string]
+  $Secret = "sampleSecret",
+
+  # 8080 is the default k8s port
+  # 5198 is the default when running F5
+  [string]
+  $BaseUrl = "http://localhost:5198"
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module ../Package-Management.psm1 -Force
+Import-Module ./modules/SmokeTest.psm1
+
+# $sdkPackagePath = Get-ApiSdk -PackageVersion '7.2.1183'
+# $sdkPath = (Join-Path -Path ($sdkPackagePath).Trim() -ChildPath "lib/net8.0/EdFi.OdsApi.Sdk.dll")
+$sdkPath = "$PSScriptRoot/sdk/EdFi.OdsApi.Sdk.dll"
+$path = Get-SmokeTestTool -PackageVersion '7.2.413'
+
+$parameters = @{
+  BaseUrl = $BaseUrl
+  Key = $Key
+  Secret = $Secret
+  ToolPath = $path
+  TestSet = "NonDestructiveSdk"
+  SdkPath = $sdkPath
+}
+
+RunApiTests @parameters
+
+
