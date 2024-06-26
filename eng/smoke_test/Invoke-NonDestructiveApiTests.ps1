ca97df4cf2c502da9b21cd96245983703b7eb72e
CSR2017 <suganya@imanisoftware.com>
2024-06-26 16:04:05 -0500

    Add non-destructive test ps1

---
+++ eng/smoke_test/Invoke-NonDestructiveApiTests.ps1
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+#Requires -Version 7
+
+param(
+  [string]
+  $Key = "sampleKey",
+
+  [string]
+  $Secret = "sampleSecret",
+
+  # 8080 is the default k8s port
+  # 5198 is the default when running F5
+  [string]
+  $BaseUrl = "http://localhost:5198"
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module ../Package-Management.psm1 -Force
+Import-Module ./modules/SmokeTest.psm1
+
+$path = Get-SmokeTestTool -PackageVersion '7.2.413'
+
+$parameters = @{
+  BaseUrl = $BaseUrl
+  Key = $Key
+  Secret = $Secret
+  ToolPath = $path
+  TestSet = "NonDestructiveApi"
+}
+
+RunApiTests @parameters
+
+
