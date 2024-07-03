098ff37d242f3cafc127d6e6f155e1b78afe992b
CSR2017 <suganya@imanisoftware.com>
2024-07-02 19:38:49 -0500

    Fix descriptor path

--- src/core/EdFi.DataManagementService.Core/Model/DescriptorDocument.cs
+++ src/core/EdFi.DataManagementService.Core/Model/DescriptorDocument.cs
@@ -16,7 +16,7 @@ namespace EdFi.DataManagementService.Core.Model;
 internal class DescriptorDocument(JsonNode _document)
 {
     // A hardcoded identity path for all descriptor document identities
-    public static readonly JsonPath DescriptorIdentityPath = new("$");
+    public static readonly JsonPath DescriptorIdentityPath = new("$.descriptor");
 
     /// <summary>
     /// Creates a new DocumentIdentity from the DescriptorDocument
