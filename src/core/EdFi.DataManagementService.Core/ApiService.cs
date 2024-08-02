ab640d6d81e780c73bc8ae69766a19a03f4c1e99
Johnny Brenes <johnny.brenes@simpat.tech>
2024-08-02 09:03:17 -0600

    fixing error after merge

--- src/core/EdFi.DataManagementService.Core/ApiService.cs
+++ src/core/EdFi.DataManagementService.Core/ApiService.cs
@@ -50,8 +50,7 @@ internal class ApiService(
                     new ParsePathMiddleware(_logger),
                     new ParseBodyMiddleware(_logger),
                     new DuplicatePropertiesMiddleware(_logger),
-                    new ValidateEndpointMiddleware(_logger)
-                ]);
+                    new ValidateEndpointMiddleware(_logger),
                     new ValidateEndpointMiddleware(_logger),
                     new RejectResourceIdentifierMiddleware(_logger)
                 ]
