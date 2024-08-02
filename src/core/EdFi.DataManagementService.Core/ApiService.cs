170d838c68064628fea18010c57310c005fd5ff2
Johnny Brenes <johnny.brenes@simpat.tech>
2024-08-02 07:40:21 -0600

    Add Duplicate Middleware to update steps

--- src/core/EdFi.DataManagementService.Core/ApiService.cs
+++ src/core/EdFi.DataManagementService.Core/ApiService.cs
@@ -130,6 +130,7 @@ internal class ApiService(
                     new ProvideApiSchemaMiddleware(_apiSchemaProvider, _logger),
                     new ParsePathMiddleware(_logger),
                     new ParseBodyMiddleware(_logger),
+                    new DuplicatePropertiesMiddleware(_logger),
                     new ValidateEndpointMiddleware(_logger)
                 ]);
 
