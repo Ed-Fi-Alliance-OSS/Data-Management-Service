f86ce70d30519ed424d3316d133f7e9be873bcdb
Johnny Brenes <johnny.brenes@simpat.tech>
2024-08-16 13:46:11 -0600

    Adding Disallow Duplicate References Middleware Tests

---
+++ src/core/EdFi.DataManagementService.Core.Tests.Unit/Middleware/DisallowDuplicateReferencesMiddlewareTests.cs
@@ -0,0 +1,289 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Text.Json.Nodes;
+using EdFi.DataManagementService.Core.ApiSchema;
+using EdFi.DataManagementService.Core.External.Frontend;
+using EdFi.DataManagementService.Core.External.Model;
+using EdFi.DataManagementService.Core.Middleware;
+using EdFi.DataManagementService.Core.Model;
+using EdFi.DataManagementService.Core.Pipeline;
+using FluentAssertions;
+using Json.Schema;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using static EdFi.DataManagementService.Core.Tests.Unit.TestHelper;
+
+namespace EdFi.DataManagementService.Core.Tests.Unit.Middleware;
+
+[TestFixture]
+public class DisallowDuplicateReferencesMiddlewareTests
+{
+    internal static ApiSchemaDocument DocRefSchemaDocument()
+    {
+        var result = new ApiSchemaBuilder()
+            .WithStartProject()
+            .WithStartResource("BellSchedule")
+            .WithStartDocumentPathsMapping()
+            .WithDocumentPathReference(
+                "ClassPeriod",
+                [
+                    new("$.classPeriodName", "$.classPeriods[*].classPeriodReference.classPeriodName"),
+                    new("$.schoolId", "$.classPeriods[*].classPeriodReference.schoolId"),
+                ]
+            )
+            .WithEndDocumentPathsMapping()
+            .WithEndResource()
+            .WithEndProject()
+            .ToApiSchemaDocument();
+
+        return result;
+    }
+    internal PipelineContext DocRefContext(FrontendRequest frontendRequest, RequestMethod method)
+    {
+        PipelineContext _context =
+            new(frontendRequest, method)
+            {
+                ApiSchemaDocument = DocRefSchemaDocument(),
+                PathComponents = new(
+                    ProjectNamespace: new("ed-fi"),
+                    EndpointName: new("bellschedules"),
+                    DocumentUuid: No.DocumentUuid
+                )
+            };
+        _context.ProjectSchema = new ProjectSchema(
+            _context.ApiSchemaDocument.FindProjectSchemaNode(new("ed-fi")) ?? new JsonObject(),
+            NullLogger.Instance
+        );
+        _context.ResourceSchema = new ResourceSchema(
+            _context.ProjectSchema.FindResourceSchemaNode(new("bellschedules")) ?? new JsonObject()
+        );
+
+        if (_context.FrontendRequest.Body != null)
+        {
+            var body = JsonNode.Parse(_context.FrontendRequest.Body);
+            if (body != null)
+            {
+                _context.ParsedBody = body;
+            }
+        }
+
+        return _context;
+    }
+
+    // All the middlewares necessary tu test DisallowDuplicateReference
+    internal static IPipelineStep ProvideMiddleware(IApiSchemaProvider provider)
+    {
+        var apiValidator = new ApiSchemaValidator(
+            new ApiSchemaSchemaProvider(NullLogger<ApiSchemaSchemaProvider>.Instance)
+        );
+        return new ApiSchemaValidationMiddleware(provider, apiValidator, NullLogger.Instance);
+    }
+    
+    internal static IPipelineStep BuildResourceInfo()
+    {
+        return new BuildResourceInfoMiddleware(NullLogger.Instance);
+    }
+
+    internal static IPipelineStep ExtractDocument()
+    {
+        return new ExtractDocumentInfoMiddleware(NullLogger.Instance);
+    }
+
+    // Middleware to test
+    internal static IPipelineStep Middleware()
+    {
+        return new DisallowDuplicateReferencesMiddleware(NullLogger.Instance);
+    }
+
+    [TestFixture]
+    public class Given_Pipeline_Context_With_Duplicate_Document_Reference
+        : DisallowDuplicateReferencesMiddlewareTests
+    {
+        private PipelineContext _context = No.PipelineContext();
+
+        [SetUp]
+        public async Task Setup()
+        {
+            var jsonBody = """
+                {
+                    "schoolReference": {
+                        "schoolId": 1
+                    },
+                    "bellScheduleName": "Test Schedule",
+                    "totalInstructionalTime": 325,
+                    "classPeriods": [
+                        {
+                            "classPeriodReference": {
+                                "classPeriodName": "01 - Traditional",
+                                "schoolId": 1
+                            }
+                        },
+                        {
+                            "classPeriodReference": {
+                                "classPeriodName": "01 - Traditional",
+                                "schoolId": 1
+                            }
+                        }
+                    ],
+                    "dates": [],
+                    "gradeLevels": []
+                }
+                """;
+
+            FrontendRequest frontEndRequest =
+                new(
+                    Path: "ed-fi/bellschedules",
+                    Body: jsonBody,
+                    QueryParameters: [],
+                    TraceId: new TraceId("")
+                );
+
+            _context = DocRefContext(frontEndRequest, RequestMethod.POST);
+
+            await ProvideMiddleware(new ApiSchemaValidationMiddlewareTests.Given_An_Api_Schema_With_Validation_Errors.Provider()).Execute(_context, NullNext);
+            await BuildResourceInfo().Execute(_context, NullNext);
+            await ExtractDocument().Execute(_context, NullNext);
+            
+            await Middleware().Execute(_context, NullNext);
+        }
+
+        [Test]
+        public void It_returns_status_400()
+        {
+            _context.FrontendResponse.StatusCode.Should().Be(400);
+        }
+
+        [Test]
+        public void It_returns_validation_error_with_duplicated_document_reference()
+        {
+            _context.FrontendResponse.Body.Should().Contain("Data Validation Failed");
+            _context
+                .FrontendResponse.Body.Should()
+                .Contain(
+                    """
+                    "validationErrors":{"$.ClassPeriod":["The 2nd item of the ClassPeriod has the same identifying values as another item earlier in the list."]}
+                    """
+                );
+        }
+    }
+
+    // Descriptor Reference evaluation
+    internal static ApiSchemaDocument DescRefSchemaDocument()
+    {
+        var result = new ApiSchemaBuilder()
+            .WithStartProject()
+            .WithStartResource("School")
+            .WithStartDocumentPathsMapping()
+            .WithDocumentPathDescriptor(
+                "GradeLevelDescriptor",
+                "$.gradeLevels[*].gradeLevelDescriptor"
+            )
+            .WithEndDocumentPathsMapping()
+            .WithEndResource()
+            .WithEndProject()
+            .ToApiSchemaDocument();
+
+        return result;
+    }
+    internal PipelineContext DescRefContext(FrontendRequest frontendRequest, RequestMethod method)
+    {
+        PipelineContext _context =
+            new(frontendRequest, method)
+            {
+                ApiSchemaDocument = DescRefSchemaDocument(),
+                PathComponents = new(
+                    ProjectNamespace: new("ed-fi"),
+                    EndpointName: new("schools"),
+                    DocumentUuid: No.DocumentUuid
+                )
+            };
+        _context.ProjectSchema = new ProjectSchema(
+            _context.ApiSchemaDocument.FindProjectSchemaNode(new("ed-fi")) ?? new JsonObject(),
+            NullLogger.Instance
+        );
+        _context.ResourceSchema = new ResourceSchema(
+            _context.ProjectSchema.FindResourceSchemaNode(new("schools")) ?? new JsonObject()
+        );
+
+        if (_context.FrontendRequest.Body != null)
+        {
+            var body = JsonNode.Parse(_context.FrontendRequest.Body);
+            if (body != null)
+            {
+                _context.ParsedBody = body;
+            }
+        }
+
+        return _context;
+    }
+
+    [TestFixture]
+    public class Given_Pipeline_Context_With_Duplicate_Descriptor_Reference
+        : DisallowDuplicateReferencesMiddlewareTests
+    {
+        private PipelineContext _context = No.PipelineContext();
+
+        [SetUp]
+        public async Task Setup()
+        {
+            string jsonBody = """
+                {
+                  "schoolId":255901001,
+                  "nameOfInstitution":"School Test",
+                  "gradeLevels": [
+                      {
+                        "gradeLevelDescriptor": "uri://ed-fi.org/GradeLevelDescriptor#Sixth grade"
+                      },
+                      {
+                        "gradeLevelDescriptor": "uri://ed-fi.org/GradeLevelDescriptor#Seven grade"
+                      },
+                      {
+                        "gradeLevelDescriptor": "uri://ed-fi.org/GradeLevelDescriptor#Seven grade"
+                      },
+                      {
+                        "gradeLevelDescriptor": "uri://ed-fi.org/GradeLevelDescriptor#Sixth grade"
+                      }
+                   ],  
+                   "educationOrganizationCategories":[
+                      {
+                         "educationOrganizationCategoryDescriptor":"uri://ed-fi.org/educationOrganizationCategoryDescriptor#School"
+                      }
+                   ]
+                }
+                """;
+
+            FrontendRequest frontEndRequest =
+                new(Path: "ed-fi/schools", Body: jsonBody, QueryParameters: [], TraceId: new TraceId(""));
+
+            _context = DescRefContext(frontEndRequest, RequestMethod.POST);
+
+            await ProvideMiddleware(new ApiSchemaValidationMiddlewareTests.Given_An_Api_Schema_With_Validation_Errors.Provider()).Execute(_context, NullNext);
+            await BuildResourceInfo().Execute(_context, NullNext);
+            await ExtractDocument().Execute(_context, NullNext);
+
+            await Middleware().Execute(_context, NullNext);
+        }
+
+        [Test]
+        public void It_returns_status_400()
+        {
+            _context.FrontendResponse.StatusCode.Should().Be(400);
+        }
+
+        [Test]
+        public void It_returns_message_body_with_failure_duplicated_descriptor()
+        {
+            _context.FrontendResponse.Body.Should().Contain("Data Validation Failed");
+            _context
+                .FrontendResponse.Body.Should()
+                .Contain(
+                    """
+                    "validationErrors":{"$.gradeLevels[*].gradeLevelDescriptor":["The 2nd item of the gradeLevels has the same identifying values as another item earlier in the list.","The 3rd item of the gradeLevels has the same identifying values as another item earlier in the list."]}
+                    """
+                );
+        }
+    }
+}
