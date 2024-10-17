00ce697cca2d20533f740584c0c1034bd20b5bda
CSR2017 <suganya@imanisoftware.com>
2024-10-17 14:41:29 -0500

    Enforce  authorization for /data endpoints

---
+++ src/dms/frontend/EdFi.DataManagementService.Frontend.AspNetCore/Configuration/IdentitySettings.cs
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using Microsoft.Extensions.Options;
+
+namespace EdFi.DataManagementService.Frontend.AspNetCore.Configuration;
+
+public class IdentitySettings
+{
+    public required string Authority { get; set; }
+    public bool RequireHttpsMetadata { get; set; }
+    public bool EnforceAuthorization { get; set; }
+    public required string Audience { get; set; }
+    public required string RoleClaimType { get; set; }
+    public required string ServiceRole { get; set; }
+}
+
+public class IdentitySettingsValidator : IValidateOptions<IdentitySettings>
+{
+    public ValidateOptionsResult Validate(string? name, IdentitySettings options)
+    {
+        if (string.IsNullOrWhiteSpace(options.Authority))
+        {
+            return ValidateOptionsResult.Fail("Missing required IdentitySettings value: Authority");
+        }
+        if (string.IsNullOrEmpty(options.Audience))
+        {
+            return ValidateOptionsResult.Fail("Missing required IdentitySettings value: Audience");
+        }
+        if (string.IsNullOrEmpty(options.RoleClaimType))
+        {
+            return ValidateOptionsResult.Fail("Missing required IdentitySettings value: RoleClaimType");
+        }
+        if (string.IsNullOrEmpty(options.ServiceRole))
+        {
+            return ValidateOptionsResult.Fail("Missing required IdentitySettings value: ServiceRole");
+        }
+        return ValidateOptionsResult.Success;
+    }
+}
