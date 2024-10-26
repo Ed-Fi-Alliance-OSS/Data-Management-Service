7a20b28f752a470fe28a48662708e2f903d4a135
Stephen A. Fuqua <stephen@safnet.com>
2024-10-26 14:46:59 -0500

    Refactor: - Log messages - Pass in the TraceId - Remove custom exception

--- src/dms/core/EdFi.DataManagementService.Core/OAuthManager.cs
+++ src/dms/core/EdFi.DataManagementService.Core/OAuthManager.cs
@@ -2,60 +2,126 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
-using Microsoft.Extensions.Logging;
+
 using System.Net;
 using System.Text;
+using System.Text.Json.Nodes;
+using EdFi.DataManagementService.Core.External.Model;
+using EdFi.DataManagementService.Core.Response;
+using Microsoft.Extensions.Logging;
 
 namespace EdFi.DataManagementService.Core;
 
 public interface IOAuthManager
 {
-    public Task<HttpResponseMessage> GetAccessTokenAsync(HttpClient httpClient, string authHeaderString, string upstreamUri);
+    public Task<HttpResponseMessage> GetAccessTokenAsync(
+        HttpClient httpClient,
+        string authHeaderString,
+        string upstreamUri,
+        TraceId traceId
+    );
 }
+
 public class OAuthManager(ILogger<OAuthManager> logger) : IOAuthManager
 {
     private readonly ILogger<OAuthManager> _logger = logger;
-    public async Task<HttpResponseMessage> GetAccessTokenAsync(HttpClient httpClient, string authHeaderString, string upstreamUri)
-    {
-        // Generate request message to send upstream
-        _logger.LogInformation("Generating Request message to send upstream");
-        HttpRequestMessage upstreamRequest = new HttpRequestMessage(HttpMethod.Post, upstreamUri);
 
-        // Verify the request header contains Authorization Basic.
-        _logger.LogInformation("Verifying Authorization Header contains 'Basic' Authentication formatting");
-        if (!authHeaderString.Contains("Basic"))
+    public async Task<HttpResponseMessage> GetAccessTokenAsync(
+        HttpClient httpClient,
+        string authHeaderString,
+        string upstreamUri,
+        TraceId traceId
+    )
+    {
+        _logger.LogInformation("GetAccessTokenAsync - {TraceId}", traceId);
+
+        if (!authHeaderString.Contains("basic", StringComparison.InvariantCultureIgnoreCase))
         {
-            _logger.LogError("Malformed Authorization Header");
-            throw new OAuthIdentityException($"Malformed Authorization Header", HttpStatusCode.BadRequest);
+            return GenerateProblemDetailResponse(
+                HttpStatusCode.BadRequest,
+                FailureResponse.ForBadRequest("Malformed Authorization header", traceId, [], [])
+            );
         }
 
-        // Forward Basic Authentication headers to upstream request.
+        HttpRequestMessage upstreamRequest = new(HttpMethod.Post, upstreamUri);
         upstreamRequest.Headers.Add("Authorization", authHeaderString);
 
         // TODO(DMS-408): Replace hard-coded with forwarded request body.
-        upstreamRequest!.Content = new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded");
-        _logger.LogInformation("Forwarding Authorization Header and Body to upstream service");
+        upstreamRequest!.Content = new StringContent(
+            "grant_type=client_credentials",
+            Encoding.UTF8,
+            "application/x-www-form-urlencoded"
+        );
 
         // In case of 5xx Error, pass 503 Service unavailable to client, otherwise forward response directly to client.
         try
         {
-            return await httpClient.SendAsync(upstreamRequest);
+            _logger.LogInformation(
+                "Forwarding proxied token request to upstream service - {TraceId}",
+                traceId
+            );
+            var response = await httpClient.SendAsync(upstreamRequest);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return await GenerateUnauthorizedResponse(traceId, response);
+            }
+
+            return response;
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Error from Upstream Service, notifying client...");
-            throw new OAuthIdentityException($"Upstream Service Unavailable", HttpStatusCode.ServiceUnavailable);
+            _logger.LogError(ex, "Error from upstream identity service - {TraceId}", traceId);
+            return GenerateProblemDetailResponse(
+                HttpStatusCode.BadGateway,
+                FailureResponse.ForGatewayError(traceId)
+            );
+        }
+
+        // Attempts to read `{ "error": "...", "error_description": "..."}` from the response
+        // body, with sensible fallback mechanism if the response is in a different format.
+        static async Task<HttpResponseMessage> GenerateUnauthorizedResponse(
+            TraceId traceId,
+            HttpResponseMessage response
+        )
+        {
+            var body = await response.Content.ReadAsStringAsync();
+            var error = "Unauthorized";
+            var errorDescription = body;
+
+            JsonNode? parsed = JsonNode.Parse(body);
+            if (parsed is not null)
+            {
+                var obj = parsed.AsObject();
+                if (obj.ContainsKey("error"))
+                {
+                    error = obj["error"]!.ToString();
+                }
+                if (obj.ContainsKey("error_description"))
+                {
+                    errorDescription = obj["error_description"]!.ToString();
+                }
+            }
+
+            return GenerateProblemDetailResponse(
+                HttpStatusCode.Unauthorized,
+                FailureResponse.ForUnauthorized(traceId, error, errorDescription)
+            );
+        }
+
+        static HttpResponseMessage GenerateProblemDetailResponse(
+            HttpStatusCode statusCode,
+            JsonNode failureResponse
+        )
+        {
+            return new HttpResponseMessage(statusCode)
+            {
+                Content = new StringContent(
+                    failureResponse.ToString(),
+                    Encoding.UTF8,
+                    "application/problem+json"
+                ),
+            };
         }
     }
-}
-
-public class OAuthIdentityException : Exception
-{
-    public OAuthIdentityException(string message) : base(message) { }
-
-    public OAuthIdentityException(string message, HttpStatusCode statusCode) : base(message)
-    {
-        StatusCode = statusCode;
-    }
-    public HttpStatusCode? StatusCode { get; set; }
 }
