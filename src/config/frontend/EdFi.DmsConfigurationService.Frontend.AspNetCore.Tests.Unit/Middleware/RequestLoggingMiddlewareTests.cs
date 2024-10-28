e2c13f4c5c992c00a1d4120199bcfa5a0f34e884
Johnny Brenes <johnny.brenes@simpat.tech>
2024-10-28 12:12:05 -0600

    CSharpier Recommendations

--- src/config/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit/Middleware/RequestLoggingMiddlewareTests.cs
+++ src/config/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit/Middleware/RequestLoggingMiddlewareTests.cs
@@ -15,13 +15,14 @@ using NUnit.Framework;
 
 namespace EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit.Middleware;
 
+[TestFixture]
 internal class RequestLoggingMiddlewareTests
 {
     [TestFixture]
     public class MiddlewareTests
     {
-        private RequestDelegate _next;
-        private ILogger<RequestLoggingMiddleware> _logger;
+        private RequestDelegate _next = null!;
+        private ILogger<RequestLoggingMiddleware> _logger = null!;
 
         [SetUp]
         public void Setup()
@@ -49,9 +50,8 @@ internal class RequestLoggingMiddlewareTests
         public async Task When_middleware_receives_validation_errors()
         {
             // Arrange
-            var validationError = "Validation exception occurred.";
-            var httpContext = new DefaultHttpContext();
-            httpContext.Response.Body = new MemoryStream();
+            string validationError = "Validation exception occurred.";
+            var httpContext = new DefaultHttpContext { Response = { Body = new MemoryStream() } };
 
             var exception = new ValidationException([new ValidationFailure { PropertyName = "test", ErrorMessage = validationError }]);
 
@@ -63,8 +63,8 @@ internal class RequestLoggingMiddlewareTests
             await middleWare.Invoke(httpContext, _logger);
 
             httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
-            var responseBody = await new StreamReader(httpContext.Response.Body).ReadToEndAsync();
-            var statusCode = httpContext.Response.StatusCode;
+            string responseBody = await new StreamReader(httpContext.Response.Body).ReadToEndAsync();
+            int statusCode = httpContext.Response.StatusCode;
 
             // Assert
             responseBody.Should().Contain(validationError);
@@ -75,9 +75,8 @@ internal class RequestLoggingMiddlewareTests
         public async Task When_middleware_receives_server_errors()
         {
             // Arrange
-            var error = "The server encountered an unexpected condition that prevented it from fulfilling the request.";
-            var httpContext = new DefaultHttpContext();
-            httpContext.Response.Body = new MemoryStream();
+            string error = "The server encountered an unexpected condition that prevented it from fulfilling the request.";
+            var httpContext = new DefaultHttpContext { Response = { Body = new MemoryStream() } };
 
             var exception = new Exception(error);
             A.CallTo(() => _next.Invoke(httpContext)).Throws(exception);
@@ -88,8 +87,8 @@ internal class RequestLoggingMiddlewareTests
             await middleWare.Invoke(httpContext, _logger);
 
             httpContext.Response.Body.Seek(0, SeekOrigin.Begin);
-            var responseBody = await new StreamReader(httpContext.Response.Body).ReadToEndAsync();
-            var statusCode = httpContext.Response.StatusCode;
+            string responseBody = await new StreamReader(httpContext.Response.Body).ReadToEndAsync();
+            int statusCode = httpContext.Response.StatusCode;
 
             // Assert
             responseBody.Should().Contain(error);
