bdf0f9435c436a46c46f22e56bfe7a087432a544
Adam Hopkins <adam.hopkins@simpat.tech>
2024-10-29 10:08:57 -0500

    Update _lastModifiedDate in document body on cascade updates

--- src/dms/backend/EdFi.DataManagementService.Backend.Postgresql.Test.Integration/UpdateTests.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.Postgresql.Test.Integration/UpdateTests.cs
@@ -672,6 +672,7 @@ public class UpdateTests : DatabaseTest
         private static readonly Guid _courseOfferingDocumentUuid = Guid.NewGuid();
         private static readonly Guid _courseOfferingReferentialIdUuid = Guid.NewGuid();
         private DateTime? _courseOfferingInsertDateTime;
+        private DateTime? _courseOfferingLastModifiedDate;
 
         private static readonly Guid _section1DocumentUuid = Guid.NewGuid();
         private static readonly Guid _section1ReferentialIdUuid = Guid.NewGuid();
@@ -712,7 +713,8 @@ public class UpdateTests : DatabaseTest
                     "localCourseCode": "ABC",
                     "sessionReference": {
                         "sessionName": "Third Quarter"
-                    }
+                    },
+                    "_lastModifiedDate": "2024-10-29T14:54:49+00:00"
                 }
                 """,
                 CreateDocumentReferences(
@@ -733,6 +735,8 @@ public class UpdateTests : DatabaseTest
 
             _courseOfferingInsertDateTime =
                 (getCourseOffingInsertResult! as GetResult.GetSuccess)!.LastModifiedDate;
+
+            _courseOfferingLastModifiedDate = (getCourseOffingInsertResult! as GetResult.GetSuccess)!.EdfiDoc["_lastModifiedDate"]!.GetValue<DateTime>();
 
             IUpsertRequest section1UpsertRequest = CreateUpsertRequest(
                 "Section",
@@ -815,7 +819,20 @@ public class UpdateTests : DatabaseTest
             (getResult! as GetResult.GetSuccess)!.DocumentUuid.Value.Should().Be(_courseOfferingDocumentUuid);
             (getResult! as GetResult.GetSuccess)!.EdfiDoc.ToJsonString().Should().Contain("Fourth Quarter");
             (getResult! as GetResult.GetSuccess)!.LastModifiedTraceId.Should().Be(traceId.Value);
+        }
+
+        [Test]
+        public async Task It_should_update_the_lastmodifieddate_in_the_document_body()
+        {
+            var getResult = await CreateGetById()
+                .GetById(
+                CreateGetRequest("CourseOffering", _courseOfferingDocumentUuid),
+                Connection!,
+                Transaction!
+                );
+
             (getResult! as GetResult.GetSuccess)!.LastModifiedDate.Should().NotBe(_courseOfferingInsertDateTime);
+            (getResult! as GetResult.GetSuccess)!.EdfiDoc["_lastModifiedDate"]!.GetValue<DateTime>().Should().NotBe(_courseOfferingLastModifiedDate);
         }
 
         [Test]
