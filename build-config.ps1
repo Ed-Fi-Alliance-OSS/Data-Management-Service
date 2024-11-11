52cb333cd971e83df0813a83ab2f8dc92db79af3
Johnny Brenes <johnny.brenes@simpat.tech>
2024-11-11 15:44:13 -0600

    Adding Config merge-or-tag and prerelease files

--- build-config.ps1
+++ build-config.ps1
@@ -51,7 +51,7 @@ param(
     # current package number is configured in the build automation tool and
     # passed to this script.
     [string]
-    $DmsConfigurationServiceVersion = "0.1",
+    $DmsCSVersion = "0.1",
 
     # .NET project build configuration, defaults to "Debug". Options are: Debug, Release.
     [string]
