0fba2072a897761d0a9f0e4edae52d9be3aafcab
CSR2017 <suganya@imanisoftware.com>
2024-11-27 18:26:44 -0600

    Add initial Resource claim validator

---
+++ src/config/datamodel/EdFi.DmsConfigurationService.DataModel/Model/ClaimSets/ResourceClaim.cs
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Text.Json.Serialization;
+
+namespace EdFi.DmsConfigurationService.DataModel.Model.ClaimSets;
+
+public class ResourceClaim
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+    public string? ParentName { get; set; }
+    public string? Name { get; set; }
+    public List<ResourceClaimAction>? Actions { get; set; }
+    [JsonIgnore]
+    public bool IsParent { get; set; }
+    public List<ClaimSetResourceClaimActionAuthStrategies?> DefaultAuthorizationStrategiesForCRUD { get; set; } = [];
+    public List<ClaimSetResourceClaimActionAuthStrategies?> AuthorizationStrategyOverridesForCRUD { get; set; } = [];
+    public List<ResourceClaim> Children { get; set; }
+
+    public ResourceClaim()
+    {
+        Children = [];
+    }
+}
+
+public class ResourceClaimAction
+{
+    public string? Name { get; set; }
+    public bool Enabled { get; set; }
+}
+
+public class ClaimSetResourceClaimActionAuthStrategies
+{
+    public IList<AuthorizationStrategy?> AuthorizationStrategies { get; set; } = [];
+}
