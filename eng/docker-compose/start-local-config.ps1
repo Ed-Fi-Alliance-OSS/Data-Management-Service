03ce8c1725d03c6a3d8ba4e98bfd721f0366dd8d
CSR2017 <suganya@imanisoftware.com>
2024-12-09 15:04:13 -0600

    Add script for starting the config service

---
+++ eng/docker-compose/start-local-config.ps1
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+[CmdletBinding()]
+param (
+    # Stop services instead of starting them
+    [Switch]
+    $d,
+
+    # Delete volumes after stopping services
+    [Switch]
+    $v,
+
+    # Environment file
+    [string]
+    $EnvironmentFile = "./.env",
+
+    # Force a rebuild
+    [Switch]
+    $r
+)
+
+$files = @(
+    "-f",
+    "postgresql.yml",
+    "-f",
+    "local-config.yml",
+    "-f",
+    "keycloak.yml"
+)
+
+if ($d) {
+    if ($v) {
+        Write-Output "Shutting down with volume delete"
+        docker compose $files down -v
+    }
+    else {
+        Write-Output "Shutting down"
+        docker compose $files down
+    }
+}
+else {
+    #docker network create dms
+
+    $upArgs = @(
+        "--detach"
+    )
+    if ($r) { $upArgs += @("--build") }
+
+    Write-Output "Starting locally-built DMS config service"
+
+    docker compose $files --env-file $EnvironmentFile up $upArgs
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "Unable to start local Docker environment, with exit code $LASTEXITCODE."
+    }
+
+    Start-Sleep 20
+    ./setup-keycloak.ps1
+}
+
+
