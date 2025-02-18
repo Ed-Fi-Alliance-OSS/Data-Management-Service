369674d15e23ea0293f8712e670c76284f8ac6fe
m.semalaiappan <m.semalaiappan@gmail.com>
2025-02-18 09:54:16 -0600

    error IDE0055: Fix formatting

--- src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql.Test.Integration/DatabaseTestBase.cs
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql.Test.Integration/DatabaseTestBase.cs
@@ -38,7 +38,7 @@ public abstract class DatabaseTestBase
                     new("dmscs", "vendornamespaceprefix"),
                     new("dmscs", "application"),
                     new("dmscs", "applicationeducationorganization"),
-                    new ("dmscs", "claimset"),
+                    new("dmscs", "claimset"),
                 ],
                 DbAdapter = DbAdapter.Postgres,
             }
