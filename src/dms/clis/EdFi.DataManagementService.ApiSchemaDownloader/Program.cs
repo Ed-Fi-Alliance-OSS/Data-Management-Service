8480a6f6e3db921b212cae6a02876aac358e9fda
m.semalaiappan <m.semalaiappan@gmail.com>
2025-02-18 18:09:22 -0600

    Added EdFi.DataManagementService.ApiSchemaDownloader.Tests.Unit Project

--- src/dms/clis/EdFi.DataManagementService.ApiSchemaDownloader/Program.cs
+++ src/dms/clis/EdFi.DataManagementService.ApiSchemaDownloader/Program.cs
@@ -3,214 +3,102 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using CommandLine;
-using NuGet.Common;
-using NuGet.Configuration;
-using NuGet.Packaging;
-using NuGet.Packaging.Core;
-using NuGet.Protocol.Core.Types;
+using EdFi.DataManagementService.ApiSchemaDownloader.Services;
+using EdFi.DataManagementService.ApiSchemaDownloader;
 
-namespace EdFi.DataManagementService.ApiSchemaDownloader
+
+var serviceCollection = new ServiceCollection();
+ConfigureServices(serviceCollection);
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+var downloader = serviceProvider.GetRequiredService<ApiSchemaDownloader>();
+
+try
 {
-    public static class Program
+    // Parse command-line arguments
+    var result = Parser.Default.ParseArguments<CommandLineOverrides>(args);
+
+    // Handle parsing errors
+    result.WithNotParsed(errors =>
     {
-        private static async Task Main(string[] args)
+        logger.LogError("Error parsing command-line arguments. Please provide valid parameters.");
+        Environment.Exit(1);
+    });
+
+    // Execute program logic if parsing is successful
+    await result.WithParsedAsync(async options =>
+    {
+        // Validate required parameters
+        if (string.IsNullOrWhiteSpace(options.PackageId))
         {
-            // Parse command-line arguments
-            var result = Parser.Default.ParseArguments<CommandLineOverrides>(args);
-
-            // Handle parsing errors
-            result.WithNotParsed(errors =>
-            {
-                Console.WriteLine("Error parsing command-line arguments. Please provide valid parameters.");
-                Environment.Exit(1);
-            });
-
-            // Execute program logic if parsing is successful
-            _ = await result.WithParsedAsync(async options =>
-            {
-                // Validate required parameters
-                if (string.IsNullOrWhiteSpace(options.PackageId))
-                {
-                    throw new ArgumentException("Error: packageId is required.");
-                }
-
-                string packageId = options.PackageId;
-                string? packageVersion = options.PackageVersion;
-                string feedUrl = options.FeedUrl;
-
-                // Output directory for the downloaded package and extracted files
-                string outputDir = Path.Combine(Path.GetTempPath(), "DownloadedPackages");
-                Directory.CreateDirectory(outputDir);
-
-                // Download the package
-                var packagePath = await DownloadNuGetPackageAsync(
-                    packageId,
-                    packageVersion,
-                    feedUrl,
-                    outputDir
-                );
-                Console.WriteLine($"Package downloaded to: {packagePath}");
-
-                Directory.CreateDirectory(options.ApiSchemaFolder);
-                ExtractApiSchemaJsonFromAssembly(packageId, packagePath, options.ApiSchemaFolder);
-                Console.WriteLine($"ApiSchema.json extracted to folder: {options.ApiSchemaFolder}");
-            });
-
-            static void ExtractApiSchemaJsonFromAssembly(
-                string packageId,
-                string packagePath,
-                string outputDir
-            )
-            {
-                // Open the NuGet package
-                using var packageReader = new PackageArchiveReader(packagePath);
-                // Find the DLL in the package
-                var dllPath = packageReader.GetFiles().FirstOrDefault(f => f.EndsWith("ApiSchema.dll"));
-                if (dllPath == null)
-                {
-                    throw new Exception("No DLL found in the package.");
-                }
-
-                // Extract the DLL to a temporary location
-                var tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-                Directory.CreateDirectory(tempDir);
-                var dllFilePath = Path.Combine(tempDir, Path.GetFileName(dllPath));
-                using (var dllStream = packageReader.GetStream(dllPath))
-                using (var fileStream = File.Create(dllFilePath))
-                {
-                    dllStream.CopyTo(fileStream);
-                }
-
-                // Load the assembly
-                var assembly = Assembly.LoadFrom(dllFilePath);
-
-                // Find all embedded JSON files that start with "ApiSchema"
-                var resourceNames = assembly
-                    .GetManifestResourceNames()
-                    .Where(name => name.StartsWith(packageId, StringComparison.OrdinalIgnoreCase))
-                    .ToList();
-                if (resourceNames == null)
-                {
-                    throw new Exception("ApiSchema.json not found as an embedded resource.");
-                }
-
-                // Extract the resource
-
-                foreach (var resourceName in resourceNames)
-                {
-                    // Determine the output file path based on the resource name
-                    string outputFilePath = Path.Combine(
-                        outputDir,
-                        resourceName.Replace($"{packageId}.", "")
-                    ); // Remove package ID prefix
-
-                    // Fix XSD file naming if the resource is an XSD file
-                    if (resourceName.EndsWith(".xsd"))
-                    {
-                        outputFilePath = outputFilePath.Replace("xsd.", "");
-                    }
-
-                    // Extract the resource to the output file
-                    using var resourceStream = assembly.GetManifestResourceStream(resourceName);
-                    using var fileStream = File.Create(outputFilePath);
-                    resourceStream?.CopyTo(fileStream);
-                }
-            }
-
-            static async Task<string> DownloadNuGetPackageAsync(
-                string packageId,
-                string? packageVersion,
-                string feedUrl,
-                string outputDir
-            )
-            {
-                // Create a NuGet repository
-                var providers = Repository.Provider.GetCoreV3();
-                var packageSource = new PackageSource(feedUrl);
-                var sourceRepository = new SourceRepository(packageSource, providers);
-
-                // Create a SourceCacheContext
-                var cacheContext = new SourceCacheContext();
-
-                // Get the package metadata resource
-                var packageMetadataResource =
-                    await sourceRepository.GetResourceAsync<PackageMetadataResource>();
-
-                // Get the latest version if packageVersion is not specified
-                if (string.IsNullOrWhiteSpace(packageVersion))
-                {
-                    var latestPackage = (
-                        await packageMetadataResource.GetMetadataAsync(
-                            packageId,
-                            includePrerelease: true,
-                            includeUnlisted: false,
-                            cacheContext,
-                            NullLogger.Instance,
-                            CancellationToken.None
-                        )
-                    )
-                        .OrderByDescending(p => p.Identity.Version)
-                        .FirstOrDefault();
-
-                    if (latestPackage == null)
-                    {
-                        throw new Exception($"No versions found for package {packageId} in the feed.");
-                    }
-
-                    packageVersion = latestPackage.Identity.Version.ToString();
-                }
-
-                var packageIdentity = new PackageIdentity(
-                    packageId,
-                    new NuGet.Versioning.NuGetVersion(packageVersion)
-                );
-
-                // Check if the specified version exists in the feed
-                var packageMetadata = (
-                    await packageMetadataResource.GetMetadataAsync(
-                        packageId,
-                        includePrerelease: true,
-                        includeUnlisted: false,
-                        cacheContext,
-                        NullLogger.Instance,
-                        CancellationToken.None
-                    )
-                ).FirstOrDefault(p => p.Identity.Version == packageIdentity.Version);
-
-                if (packageMetadata == null)
-                {
-                    throw new Exception(
-                        $"Package {packageId} version {packageVersion} not found in the feed."
-                    );
-                }
-
-                // Download the package
-                var downloadResource = await sourceRepository.GetResourceAsync<DownloadResource>();
-                var downloadResult = await downloadResource.GetDownloadResourceResultAsync(
-                    packageIdentity,
-                    new PackageDownloadContext(cacheContext),
-                    outputDir,
-                    NullLogger.Instance,
-                    CancellationToken.None
-                );
-
-                if (downloadResult.Status != DownloadResourceResultStatus.Available)
-                {
-                    throw new Exception($"Failed to download package {packageId} version {packageVersion}.");
-                }
-
-                // Save the package to the output directory
-                var packageFilePath = Path.Combine(outputDir, $"{packageId}.{packageVersion}.nupkg");
-                using (var fileStream = File.Create(packageFilePath))
-                {
-                    await downloadResult.PackageStream.CopyToAsync(fileStream);
-                }
-
-                Console.WriteLine($"Downloaded package: {packageId} Version: {packageVersion}");
-                return packageFilePath;
-            }
+            logger.LogCritical("Error: packageId is required.");
         }
+
+        string packageId = options.PackageId;
+        string? packageVersion = options.PackageVersion;
+        string feedUrl = options.FeedUrl;
+
+        // Output directory for the downloaded package and extracted files
+        string outputDir = Path.Combine(Path.GetTempPath(), "DownloadedPackages");
+        Directory.CreateDirectory(outputDir);
+
+        // Download the package
+        string packagePath = await downloader.DownloadNuGetPackageAsync(
+            packageId,
+            packageVersion,
+            feedUrl,
+            outputDir
+        );
+        Console.WriteLine($"Package downloaded to: {packagePath}");
+        logger.LogInformation("Package downloaded to: $packagePath");
+
+        // Ensure the output directory for API schema extraction exists
+        Directory.CreateDirectory(options.ApiSchemaFolder);
+
+        // Extract the API schema
+        downloader.ExtractApiSchemaJsonFromAssembly(packageId, packagePath, options.ApiSchemaFolder);
+        Console.WriteLine($"ApiSchema.json extracted to folder: {options.ApiSchemaFolder}");
+        logger.LogInformation("ApiSchema.json extracted to folder: $options.ApiSchemaFolder");
+    });
+
+    return 0;
+}
+catch (Exception ex)
+{
+    logger.LogCritical(ex, "An error occurred while generating the OpenAPI spec.");
+    return 1;
+}
+
+void ConfigureServices(IServiceCollection services)
+{
+    var logConfiguration = new LoggerConfiguration().MinimumLevel.Debug();
+
+    if (Console.IsOutputRedirected)
+    {
+        logConfiguration.WriteTo.File("logs/ApiSchemaDownloader.log", rollingInterval: RollingInterval.Day);
     }
+    else
+    {
+        logConfiguration.WriteTo.Console();
+        logConfiguration.WriteTo.File("logs/ApiSchemaDownloader.log", rollingInterval: RollingInterval.Day);
+    }
+
+    Log.Logger = logConfiguration.CreateLogger();
+
+    services.AddLogging(config =>
+    {
+        config.ClearProviders();
+        config.AddSerilog();
+    });
+
+    services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+
+    services.AddSingleton<IApiSchemaDownloader, ApiSchemaDownloader>();
+
 }
