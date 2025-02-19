ea38d8f4c92b7ccc8df2f25f59ae17a4187ad2c5
CSR2017 <suganya@imanisoftware.com>
2025-02-19 09:22:16 -0600

    Add unit tests

---
+++ src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Security/AuthorizationValidation/RelationshipsWithEdOrgsOnlyValidatorTests.cs
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.DataManagementService.Core.External.Model;
+using EdFi.DataManagementService.Core.Security;
+using EdFi.DataManagementService.Core.Security.AuthorizationValidation;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EdFi.DataManagementService.Core.Tests.Unit.Security.AuthorizationValidation;
+
+public class RelationshipsWithEdOrgsOnlyValidatorTests
+{
+    [TestFixture]
+    public class Given_Request_Has_No_EducationOrganizations
+    {
+        private AuthorizationResult? _expectedResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            var validator = new RelationshipsWithEdOrgsOnlyValidator();
+            _expectedResult = validator.ValidateAuthorization(
+                new DocumentSecurityElements([], []),
+                new ClientAuthorizations(
+                    "",
+                    "",
+                    [new EducationOrganizationId("299501")],
+                    [new NamespacePrefix("uri://namespace")]
+                )
+            );
+        }
+
+        [Test]
+        public void Should_Return_Expected_AuthorizationResult()
+        {
+            _expectedResult.Should().NotBeNull();
+            _expectedResult!.IsAuthorized.Should().BeFalse();
+            _expectedResult!.ErrorMessage.Should().NotBeEmpty();
+            _expectedResult!
+                .ErrorMessage.Should()
+                .Be(
+                    "No 'EducationOrganizationIds' property could be found on the resource in order to perform authorization. Should a different authorization strategy be used?"
+                );
+        }
+    }
+
+    [TestFixture]
+    public class Given_Claim_Has_No_EducationOrganizations
+    {
+        private AuthorizationResult? _expectedResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            var validator = new RelationshipsWithEdOrgsOnlyValidator();
+            _expectedResult = validator.ValidateAuthorization(
+                new DocumentSecurityElements([], ["255901"]),
+                new ClientAuthorizations("", "", [], [])
+            );
+        }
+
+        [Test]
+        public void Should_Return_Expected_AuthorizationResult()
+        {
+            _expectedResult.Should().NotBeNull();
+            _expectedResult!.IsAuthorized.Should().BeFalse();
+            _expectedResult!.ErrorMessage.Should().NotBeEmpty();
+            _expectedResult!
+                .ErrorMessage.Should()
+                .Be(
+                    $"The API client has been given permissions on a resource that uses the 'RelationshipsWithEdOrgsOnly' authorization strategy but the client doesn't have any education organizations assigned."
+                );
+        }
+    }
+
+    [TestFixture]
+    public class Given_Matching_EducationOrganizations_Between_Request_And_Claim
+    {
+        private AuthorizationResult? _expectedResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            var validator = new RelationshipsWithEdOrgsOnlyValidator();
+            _expectedResult = validator.ValidateAuthorization(
+                new DocumentSecurityElements([], ["255901"]),
+                new ClientAuthorizations("", "", [new EducationOrganizationId("255901")], [])
+            );
+        }
+
+        [Test]
+        public void Should_Return_Success_AuthorizationResult()
+        {
+            _expectedResult.Should().NotBeNull();
+            _expectedResult!.IsAuthorized.Should().BeTrue();
+            _expectedResult!.ErrorMessage.Should().BeEmpty();
+        }
+    }
+
+    [TestFixture]
+    public class Given_Non_Matching_EducationOrganization_Between_Request_And_Claim
+    {
+        private AuthorizationResult? _expectedResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            var validator = new RelationshipsWithEdOrgsOnlyValidator();
+            _expectedResult = validator.ValidateAuthorization(
+                new DocumentSecurityElements(["uri://not-matching/resource"], ["289766"]),
+                new ClientAuthorizations(
+                    "",
+                    "",
+                    [new EducationOrganizationId("2455")],
+                    [new NamespacePrefix("uri://namespace")]
+                )
+            );
+        }
+
+        [Test]
+        public void Should_Return_Expected_AuthorizationResult()
+        {
+            _expectedResult.Should().NotBeNull();
+            _expectedResult!.IsAuthorized.Should().BeFalse();
+            _expectedResult!.ErrorMessage.Should().NotBeEmpty();
+            _expectedResult!
+                .ErrorMessage.Should()
+                .Be(
+                    $"No relationships have been established between the caller's education organization id claims ('2455') and properties of the resource item."
+                );
+        }
+    }
+
+    [TestFixture]
+    public class Given_Non_Matching_EducationOrganizations_Between_Request_And_Claim
+    {
+        private AuthorizationResult? _expectedResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            var validator = new RelationshipsWithEdOrgsOnlyValidator();
+            _expectedResult = validator.ValidateAuthorization(
+                new DocumentSecurityElements([], ["233", "244"]),
+                new ClientAuthorizations(
+                    "",
+                    "",
+                    [new EducationOrganizationId("566"), new EducationOrganizationId("567")],
+                    []
+                )
+            );
+        }
+
+        [Test]
+        public void Should_Return_Expected_AuthorizationResult()
+        {
+            _expectedResult.Should().NotBeNull();
+            _expectedResult!.IsAuthorized.Should().BeFalse();
+            _expectedResult!.ErrorMessage.Should().NotBeEmpty();
+            _expectedResult!
+                .ErrorMessage.Should()
+                .Be(
+                    $"No relationships have been established between the caller's education organization id claims ('566', '567') and properties of the resource item."
+                );
+        }
+    }
+}
