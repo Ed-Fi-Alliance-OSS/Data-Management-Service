654c8c62dbd10fcf0fccf23b201541107e24bf2c
Adam Hopkins <adam.hopkins@simpat.tech>
2025-02-25 11:49:07 -0600

    Backend to populate EducationOrganizationHierarchy table

---
+++ src/dms/core/EdFi.DataManagementService.Core.External/Model/EducationOrganizationHierarchyInfo.cs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.DataManagementService.Core.External.Model;
+
+/// <summary>
+/// Information about a resource in the education organization hierarchy
+/// </summary>
+public record EducationOrganizationHierarchyInfo(
+    bool IsInEducationOrganizationHierarchy,
+    string EducationOrganizationId,
+    string? ParentEducationOrganizationId
+);
