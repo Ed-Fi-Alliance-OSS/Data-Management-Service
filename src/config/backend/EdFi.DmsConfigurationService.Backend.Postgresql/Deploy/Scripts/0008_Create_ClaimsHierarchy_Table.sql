28492e3277f4f994a2234dbee3e5a13463d0b6b3
Johnny Brenes <johnny.brenes@simpat.tech>
2025-03-03 15:16:31 -0600

    Table creation and generate data for Aut Strategy ad Resource Claims

---
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0008_Create_ClaimsHierarchy_Table.sql
@@ -0,0 +1,18 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+create table dmscs.ClaimsHierarchy (
+  Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+  ProjectName VARCHAR(20) NOT NULL,
+  Version VARCHAR(10) NOT NULL,
+  Hierarchy jsonb NOT NULL
+);
+
+CREATE UNIQUE INDEX uq_ProjectName_Version ON ClaimsHierarchy USING btree (ProjectName, Version);
+CREATE UNIQUE INDEX idx_ProjectName_Version ON ClaimsHierarchy USING btree (ProjectName, Version);
+COMMENT ON COLUMN dmscs.ClaimsHierarchy.Id IS 'Claims hierarchy internal identifier.';
+COMMENT ON COLUMN dmscs.ClaimsHierarchy.ProjectName IS 'Project name, also known as the schema, of the model represented by the hierarchy.';
+COMMENT ON COLUMN dmscs.ClaimsHierarchy.Version IS 'The version associated with the project/schema (e.g. 1.0.0, 1.1.0, 4.0.0, 5.2.0, etc.).';
+COMMENT ON COLUMN dmscs.ClaimsHierarchy.Hierarchy IS 'Contains the JSON representation of the hierarchy of resource claims defined in the model.';
---
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0009_Insert_AuthorizationStrategy.sql
@@ -0,0 +1,24 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+ALTER TABLE dmscs.AuthorizationStrategy ALTER COLUMN Id DROP IDENTITY;
+
+INSERT INTO dmscs.AuthorizationStrategy (Id, AuthorizationStrategyName, DisplayName)
+VALUES
+    (1, 'NoFurtherAuthorizationRequired', 'No Further Authorization Required'),
+    (2, 'RelationshipsWithEdOrgsAndPeople', 'Relationships with Education Organizations and People'),
+    (3, 'RelationshipsWithEdOrgsOnly', 'Relationships with Education Organizations only'),
+    (4, 'NamespaceBased', 'Namespace Based'),
+    (5, 'RelationshipsWithPeopleOnly', 'Relationships with People only'),
+    (6, 'RelationshipsWithStudentsOnly', 'Relationships with Students only'),
+    (7, 'RelationshipsWithStudentsOnlyThroughResponsibility', 'Relationships with Students only (through StudentEducationOrganizationResponsibilityAssociation)'),
+    (8, 'OwnershipBased', 'Ownership Based'),
+    (9, 'RelationshipsWithEdOrgsAndPeopleIncludingDeletes', 'Relationships with Education Organizations and People (including deletes)'),
+    (10, 'RelationshipsWithStudentsOnlyIncludingDeletes', 'Relationships With Students Only Including Deletes'),
+    (11, 'RelationshipsWithEdOrgsOnlyInverted', 'Relationships with Education Organizations only (Inverted)'),
+    (12, 'RelationshipsWithEdOrgsAndPeopleInverted', 'Relationships with Education Organizations and People (Inverted)'),
+    (13, 'RelationshipsWithStudentsOnlyThroughResponsibilityIncludingDeletes', 'Relationships with Students only (through StudentEducationOrganizationResponsibilityAssociation, including deletes)');
+
+ALTER TABLE dmscs.AuthorizationStrategy ALTER COLUMN Id ADD GENERATED ALWAYS AS IDENTITY;
