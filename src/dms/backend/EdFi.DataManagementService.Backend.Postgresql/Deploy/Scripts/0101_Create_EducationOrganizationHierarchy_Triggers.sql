25da711bff00bf0077de7710b98c84798084d9f8
Adam Hopkins <adam.hopkins@simpat.tech>
2025-03-03 10:48:18 -0600

    Fix shallow hierarchy problem in proc. Clean up RestClient test file

--- src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/Deploy/Scripts/0101_Create_EducationOrganizationHierarchy_Triggers.sql
+++ src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/Deploy/Scripts/0101_Create_EducationOrganizationHierarchy_Triggers.sql
@@ -14,44 +14,107 @@ BEGIN
     INSERT INTO dms.EducationOrganizationHierarchyTermsLookup(Id, Hierarchy)
     VALUES (NEW.EducationOrganizationId, jsonb_build_array(NEW.EducationOrganizationId));
 
-    -- Add this item to the end of its parent's hierarchy array
+    -- Find all ancestors of the new education organization using recursive CTE
+    WITH RECURSIVE ancestors AS (
+        -- Start with the direct parent
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        WHERE h.Id = NEW.ParentId
+
+        UNION ALL
+
+        -- Recursively get all parents up the hierarchy
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        JOIN ancestors a ON h.Id = a.ParentId
+    )
+    -- Update the hierarchy for all ancestors
     UPDATE dms.EducationOrganizationHierarchyTermsLookup
     SET Hierarchy = jsonb_insert(
         Hierarchy,
-        '{-1}',
-        to_jsonb(NEW.EducationOrganizationId), true)
-    WHERE Id = (SELECT EducationOrganizationId FROM dms.EducationOrganizationHierarchy WHERE Id = NEW.ParentId);
- ELSIF( TG_OP = 'UPDATE' AND NEW.ParentId <> OLD.ParentId ) THEN
-    -- Remove from the old parent
+        '{-1}',  -- Insert at the end
+        to_jsonb(NEW.EducationOrganizationId),
+        true
+    )
+    WHERE Id IN (SELECT EducationOrganizationId FROM ancestors);
+
+ ELSIF (TG_OP = 'UPDATE' AND NEW.ParentId <> OLD.ParentId) THEN
+    -- Find all ancestors of the old parent
+    WITH RECURSIVE old_ancestors AS (
+        -- Start with the direct old parent
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        WHERE h.Id = OLD.ParentId
+
+        UNION ALL
+
+        -- Recursively get all parents up the hierarchy
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        JOIN old_ancestors a ON h.Id = a.ParentId
+    )
+    -- Remove from all old ancestors' hierarchies
     UPDATE dms.EducationOrganizationHierarchyTermsLookup
     SET Hierarchy = (
-            SELECT jsonb_agg(elem)
-            FROM jsonb_array_elements(Hierarchy) elem
-            WHERE elem <> to_jsonb(OLD.EducationOrganizationId)
-        )
-    WHERE Id = (SELECT EducationOrganizationId FROM dms.EducationOrganizationHierarchy WHERE Id = OLD.ParentId);
+        SELECT jsonb_agg(elem)
+        FROM jsonb_array_elements(Hierarchy) elem
+        WHERE elem <> to_jsonb(OLD.EducationOrganizationId)
+    )
+    WHERE Id IN (SELECT EducationOrganizationId FROM old_ancestors);
 
-    -- Add to the new parent
+    -- Find all ancestors of the new parent
+    WITH RECURSIVE new_ancestors AS (
+        -- Start with the direct new parent
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        WHERE h.Id = NEW.ParentId
+
+        UNION ALL
+
+        -- Recursively get all parents up the hierarchy
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        JOIN new_ancestors a ON h.Id = a.ParentId
+    )
+    -- Add to all new ancestors' hierarchies
     UPDATE dms.EducationOrganizationHierarchyTermsLookup
     SET Hierarchy = jsonb_insert(
         Hierarchy,
-        '{-1}',
-        to_jsonb(NEW.EducationOrganizationId), true)
-    WHERE Id = (SELECT EducationOrganizationId FROM dms.EducationOrganizationHierarchy WHERE Id = NEW.ParentId);
+        '{-1}',  -- Insert at the end
+        to_jsonb(NEW.EducationOrganizationId),
+        true
+    )
+    WHERE Id IN (SELECT EducationOrganizationId FROM new_ancestors);
+
  ELSIF (TG_OP = 'DELETE') THEN
     -- Delete this record
-	DELETE FROM dms.EducationOrganizationHierarchyTermsLookup
-	WHERE Id = OLD.EducationOrganizationId;
+    DELETE FROM dms.EducationOrganizationHierarchyTermsLookup
+    WHERE Id = OLD.EducationOrganizationId;
 
-    -- Remove the deleted item from its parent's hierarchy array
-	UPDATE dms.EducationOrganizationHierarchyTermsLookup
+    -- Find all ancestors of the deleted organization
+    WITH RECURSIVE ancestors AS (
+        -- Start with the direct parent
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        WHERE h.Id = OLD.ParentId
+
+        UNION ALL
+
+        -- Recursively get all parents up the hierarchy
+        SELECT h.Id, h.EducationOrganizationId, h.ParentId
+        FROM dms.EducationOrganizationHierarchy h
+        JOIN ancestors a ON h.Id = a.ParentId
+    )
+    -- Remove the deleted item from all ancestors' hierarchy arrays
+    UPDATE dms.EducationOrganizationHierarchyTermsLookup
     SET Hierarchy = (
-            SELECT jsonb_agg(elem)
-            FROM jsonb_array_elements(Hierarchy) elem
-            WHERE elem <> to_jsonb(OLD.EducationOrganizationId)
-        )
-    WHERE Id = (SELECT EducationOrganizationId FROM dms.EducationOrganizationHierarchy WHERE Id = OLD.ParentId);
+        SELECT jsonb_agg(elem)
+        FROM jsonb_array_elements(Hierarchy) elem
+        WHERE elem <> to_jsonb(OLD.EducationOrganizationId)
+    )
+    WHERE Id IN (SELECT EducationOrganizationId FROM ancestors);
  END IF;
+
 RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
@@ -59,4 +122,4 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER EducationOrganizationHierarchyTrigger
 AFTER INSERT OR UPDATE OR DELETE ON dms.EducationOrganizationHierarchy
     FOR EACH ROW
-    EXECUTE PROCEDURE dms.EducationOrganizationHierarchyTriggerFunction()
+    EXECUTE PROCEDURE dms.EducationOrganizationHierarchyTriggerFunction();
