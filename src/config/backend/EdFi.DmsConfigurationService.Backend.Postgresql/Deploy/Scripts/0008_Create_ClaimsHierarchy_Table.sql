023c126965c16dd2780dc0de46a7b34e1a320d08
Johnny Brenes <johnny.brenes@simpat.tech>
2025-03-05 17:13:45 -0600

    Adding Script for claimhierarchy

--- src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0008_Create_ClaimsHierarchy_Table.sql
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0008_Create_ClaimsHierarchy_Table.sql
@@ -5,13 +5,9 @@
 
 CREATE TABLE dmscs.ClaimsHierarchy (
   Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
-  ProjectName VARCHAR(20) NOT NULL,
-  Version VARCHAR(10) NOT NULL,
-  Hierarchy JSONB NOT NULL
+  Hierarchy JSONB NOT NULL,
+  LastModifiedDate TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX idx_ProjectName_Version ON dmscs.ClaimsHierarchy USING btree (ProjectName, Version);
 COMMENT ON COLUMN dmscs.ClaimsHierarchy.Id IS 'Claims hierarchy internal identifier.';
-COMMENT ON COLUMN dmscs.ClaimsHierarchy.ProjectName IS 'Project name, also known as the schema, of the model represented by the hierarchy.';
-COMMENT ON COLUMN dmscs.ClaimsHierarchy.Version IS 'The version associated with the project/schema (e.g. 1.0.0, 1.1.0, 4.0.0, 5.2.0, etc.).';
 COMMENT ON COLUMN dmscs.ClaimsHierarchy.Hierarchy IS 'Contains the JSON representation of the hierarchy of resource claims defined in the model.';
