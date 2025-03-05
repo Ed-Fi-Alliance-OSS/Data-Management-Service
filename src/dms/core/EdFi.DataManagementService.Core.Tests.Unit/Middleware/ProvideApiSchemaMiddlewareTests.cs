ab33dc9bd21d0e4e5963027a2321637ff4b41d37
m.semalaiappan <m.semalaiappan@gmail.com>
2025-03-04 22:21:57 -0600

    ProvideApiSchemaMiddlewareTests Updated

--- src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Middleware/ProvideApiSchemaMiddlewareTests.cs
+++ src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Middleware/ProvideApiSchemaMiddlewareTests.cs
@@ -29,30 +29,98 @@ public class ProvideApiSchemaMiddlewareTests
         private readonly PipelineContext _context = No.PipelineContext();
         private static readonly JsonNode _apiSchemaRootNode =
             JsonNode.Parse(
-                "{\"projectSchema\": { \"abstractResources\":{},\"caseInsensitiveEndpointNameMapping\":{},\"description\":\"The Ed-Fi Data Standard v5.0\",\"isExtensionProject\":false,\"projectName\":\"ed-fi\",\"projectEndpointName\":\"ed-fi\",\"projectVersion\":\"5.0.0\",\"resourceNameMapping\":{},\"resourceSchemas\":{}} }"
+                "{\"projectSchema\": { "
+                    + "\"abstractResources\": {}, "
+                    + "\"caseInsensitiveEndpointNameMapping\": {}, "
+                    + "\"description\": \"The Ed-Fi Data Standard v5.0\", "
+                    + "\"isExtensionProject\": false, "
+                    + "\"projectName\": \"ed-fi\", "
+                    + "\"projectEndpointName\": \"ed-fi\", "
+                    + "\"projectVersion\": \"5.0.0\", "
+                    + "\"resourceNameMapping\": {}, "
+                    + "\"resourceSchemas\": {"
+                    + "\"credentials\": {"
+                    + "\"isResourceExtension\": true, "
+                    + "\"dateTimeJsonPaths\": [\"/dateTime/path1\"], "
+                    + "\"booleanJsonPaths\": [\"/boolean/path1\"]"
+                    + "}}}}"
+            ) ?? new JsonObject();
+
+        private static readonly JsonNode _extensionApiSchemaRootNode =
+            JsonNode.Parse(
+                "{\"projectSchema\": { "
+                    + "\"resourceSchemas\": { "
+                    + "\"credentials\": { "
+                    + "\"isResourceExtension\": true, "
+                    + "\"dateTimeJsonPaths\": [\"/dateTime/path1\"], "
+                    + "\"booleanJsonPaths\": [\"/boolean/path1\"] "
+                    + "} "
+                    + "} "
+                    + "}}"
             ) ?? new JsonObject();
 
         internal class Provider : IApiSchemaProvider
         {
             public ApiSchemaNodes GetApiSchemaNodes()
             {
-                return new(_apiSchemaRootNode, []);
+                return new ApiSchemaNodes(_apiSchemaRootNode, new[] { _extensionApiSchemaRootNode });
             }
         }
 
         [SetUp]
         public async Task Setup()
         {
-            await ProvideMiddleware(new Provider()).Execute(_context, NullNext);
+            var provider = new Provider();
+            var apiSchemaNodes = provider.GetApiSchemaNodes();
+
+            _context.ApiSchemaDocuments = new ApiSchemaDocuments(apiSchemaNodes, NullLogger.Instance);
+
+            var middleware = ProvideMiddleware(provider);
+            await middleware.Execute(_context, NullNext);
         }
 
         [Test]
         public void It_has_the_root_node_from_the_provider()
         {
+            _context.Should().NotBeNull();
+            _context.ApiSchemaDocuments.Should().NotBeNull();
+
             _context
                 .ApiSchemaDocuments.FindProjectSchemaForProjectNamespace(new("ed-fi"))
                 .Should()
                 .NotBeNull();
+        }
+
+        [Test]
+        public void It_updates_core_resource_schemas_with_extension_json_paths()
+        {
+            var provider = new Provider();
+            var apiSchemaNodes = provider.GetApiSchemaNodes();
+
+            var coreResourceSchemas = apiSchemaNodes
+                .CoreApiSchemaRootNode
+                ?["projectSchema"]
+                ?["resourceSchemas"];
+            coreResourceSchemas.Should().NotBeNull();
+
+            var extensionResourceSchema = apiSchemaNodes
+                .ExtensionApiSchemaRootNodes[0]
+                ?["projectSchema"]
+                ?["resourceSchemas"]
+                ?["credentials"];
+            extensionResourceSchema.Should().NotBeNull();
+
+            var dateTimeJsonPaths = extensionResourceSchema?["dateTimeJsonPaths"] as JsonArray;
+            var booleanJsonPaths = extensionResourceSchema?["booleanJsonPaths"] as JsonArray;
+
+            dateTimeJsonPaths.Should().NotBeNull();
+            booleanJsonPaths.Should().NotBeNull();
+
+            var dateTimePaths = dateTimeJsonPaths?.Select(p => p?.ToString()).ToList();
+            var booleanPaths = booleanJsonPaths?.Select(p => p?.ToString()).ToList();
+
+            dateTimePaths.Should().Contain("/dateTime/path1");
+            booleanPaths.Should().Contain("/boolean/path1");
         }
     }
 }
