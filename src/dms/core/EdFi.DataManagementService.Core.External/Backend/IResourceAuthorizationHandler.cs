b446e43d4c7300e678fa49f3cb3fdd9f11977121
Adam Hopkins <adam.hopkins@simpat.tech>
2025-03-11 10:28:38 -0500

    Comments

--- src/dms/core/EdFi.DataManagementService.Core.External/Backend/IResourceAuthorizationHandler.cs
+++ src/dms/core/EdFi.DataManagementService.Core.External/Backend/IResourceAuthorizationHandler.cs
@@ -3,8 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Text.Json.Nodes;
-
 namespace EdFi.DataManagementService.Core.External.Backend;
 
 /// <summary>
@@ -12,7 +10,18 @@ namespace EdFi.DataManagementService.Core.External.Backend;
 /// </summary>
 public interface IResourceAuthorizationHandler
 {
+    /// <summary>
+    /// Determine whether the authorization conditions are met for the given
+    /// namespaces and educationOrganizationIds
+    /// </summary>
+    /// <returns></returns>
     ResourceAuthorizationResult Authorize(string[] namespaces, long[] educationOrganizationIds);
 
+    /// <summary>
+    /// Returns whether this handler expects to evaluate education
+    /// organizations, based on the authorization strategy used to create it. We
+    /// won't bother to hydrate the education organization hierarchy data if
+    /// this is false.
+    /// </summary>
     bool IsRelationshipWithEdOrg { get; }
 }
