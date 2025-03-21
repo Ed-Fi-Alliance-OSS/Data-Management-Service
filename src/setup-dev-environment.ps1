1dbd4bb55b1fcd008393523872ba20c662bb7a2e
Johnny Brenes <johnny.brenes@simpat.tech>
2025-03-21 12:04:40 -0600

    Relocating some files

--- src/setup-dev-environment.ps1
+++ src/setup-dev-environment.ps1
@@ -11,10 +11,6 @@ Script to set up the development environment for the repository.
 This script restores .NET tools and installs Husky for managing Git hooks.
 #>
 
-# Move to the root of the Git repository
-$repoRoot = git rev-parse --show-toplevel
-Set-Location $repoRoot
-
 Write-Host "Setting up the development environment..."
 
 # Restore .NET tools
