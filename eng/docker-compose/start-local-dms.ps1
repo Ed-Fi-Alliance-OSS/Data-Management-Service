247f3e23cf9dd9867bb3a64de1f55de4e7883168
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2025-04-07 15:18:46 -0500

    [DMS-612] Docker Compose Optimization (#472)
    * Add file headers to docker compose files
    
    * Use a defined project name for improved grouping of containers
    
    * dockercompose language header config
    
    * Re-arrange to standard convention as recommended by dclint
    Default realm is `edfi`, not `dms`
    Limit port mapping to be available only on the localhost, and enclose port in quotes for better YAML parseability
    
    * Simple HTTP script for verifying services are running
    
    * Clarify DMS HTTP Ports env var name
    
    * Reorganize files
    Limit port exposure to localhost
    
    * Fill in missing digest information
    
    * Typo fix
    
    * Extend the wait period to match the DMS startup's 25 seconds
    
    * Rename the CS "project" and use in the `down` statements
    
    * Also need the project name on shutdown in the dms startup

--- eng/docker-compose/start-local-dms.ps1
+++ eng/docker-compose/start-local-dms.ps1
@@ -64,15 +64,18 @@ if ($EnableConfig) {
 if ($d) {
     if ($v) {
         Write-Output "Shutting down with volume delete"
-        docker compose $files down -v
+        docker compose $files -p dms-local down -v
     }
     else {
         Write-Output "Shutting down"
-        docker compose $files down
+        docker compose $files -p dms-local down
     }
 }
 else {
-    docker network create dms
+    $existingNetwork = docker network ls --filter name="dms" -q
+    if (! $existingNetwork) {
+        docker network create dms
+    }
 
     $upArgs = @(
         "--detach"
@@ -81,7 +84,7 @@ else {
 
     Write-Output "Starting locally-built DMS"
 
-    docker compose $files --env-file $EnvironmentFile up $upArgs
+    docker compose $files --env-file $EnvironmentFile -p dms-local up $upArgs
 
     if ($LASTEXITCODE -ne 0) {
         throw "Unable to start local Docker environment, with exit code $LASTEXITCODE."
