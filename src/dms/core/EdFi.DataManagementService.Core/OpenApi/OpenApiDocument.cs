1126e345f7df3d9b1bdb18257456c7a0a62b549a
Johnny Brenes <johnny.brenes@simpat.tech>
2025-04-28 11:13:39 -0600

    change the way in wich extensions are build

--- src/dms/core/EdFi.DataManagementService.Core/OpenApi/OpenApiDocument.cs
+++ src/dms/core/EdFi.DataManagementService.Core/OpenApi/OpenApiDocument.cs
@@ -19,7 +19,11 @@ public class OpenApiDocument(ILogger _logger)
     /// Inserts exts from extension OpenAPI fragments into the _ext section of the corresponding
     /// core OpenAPI endpoint.
     /// </summary>
-    private void InsertExts(JsonObject openApiExtensionFragmentList, JsonNode openApiCoreResources)
+    private void InsertExts(
+        JsonObject openApiExtensionFragmentList,
+        JsonNode openApiCoreResources,
+        string? projectName
+    )
     {
         foreach ((string componentSchemaName, JsonNode? extObject) in openApiExtensionFragmentList)
         {
@@ -39,17 +43,59 @@ public class OpenApiDocument(ILogger _logger)
                     $"OpenAPI extension fragment expects Core to have '$.components.schemas.{componentSchemaName}.properties'. Extension fragment validation failed?"
                 );
 
-            // If _ext has already been added by another extension, we don't support a second one
-            if (locationForExt["_ext"] != null)
+            string extensionSchemaName = $"{componentSchemaName}Extensions";
+
+            // Add _ext if is not already there
+            if (locationForExt["_ext"] == null)
             {
-                _logger.LogError(
-                    "OpenAPI extension fragment tried to add a second _ext to '$.components.schemas.{ComponentSchemaName}.properties', which is not supported. Extension fragment validation failed?",
-                    componentSchemaName
+                locationForExt.Add(
+                    "_ext",
+                    JsonNode.Parse($"{{ \"$ref\": \"#/components/schemas/{extensionSchemaName}\" }}")
                 );
             }
-            else
+
+            JsonObject componentsSchemas =
+                openApiCoreResources.SelectNodeFromPath("$.components.schemas", _logger)?.AsObject()
+                ?? throw new InvalidOperationException(
+                    $"OpenAPI core resources missing 'components.schemas'."
+                );
+
+            if (!componentsSchemas.ContainsKey(extensionSchemaName))
             {
-                locationForExt.Add("_ext", extObject.DeepClone());
+                componentsSchemas.Add(
+                    extensionSchemaName,
+                    new JsonObject { ["type"] = "object", ["properties"] = new JsonObject() }
+                );
+            }
+
+            JsonObject extensionSchema =
+                componentsSchemas[extensionSchemaName]?.AsObject()
+                ?? throw new InvalidOperationException(
+                    $"Extension schema '{extensionSchemaName}' is not an object."
+                );
+
+            JsonObject extensionProperties =
+                extensionSchema["properties"]?.AsObject()
+                ?? throw new InvalidOperationException(
+                    $"Extension schema '{extensionSchemaName}' missing 'properties'."
+                );
+
+            // Name of the specific project schema
+            string projectExtensionSchemaName = $"{projectName}_{componentSchemaName}Extension";
+
+            // Add reference to the specific project schema
+            if (!extensionProperties.ContainsKey(projectName!))
+            {
+                extensionProperties.Add(
+                    projectName!,
+                    JsonNode.Parse($"{{ \"$ref\": \"#/components/schemas/{projectExtensionSchemaName}\" }}")
+                );
+            }
+
+            // Add the specific project schema if it doesn't exist
+            if (!componentsSchemas.ContainsKey(projectExtensionSchemaName))
+            {
+                componentsSchemas.Add(projectExtensionSchemaName, extObject.DeepClone());
             }
         }
     }
@@ -76,15 +122,12 @@ public class OpenApiDocument(ILogger _logger)
             // If pathName has already been added by another extension, we don't support a second one
             if (locationForPaths[pathName] != null)
             {
-                _logger.LogError(
-                    "OpenAPI extension fragment tried to add a second path '$.paths.{PathName}', which is not supported. Extension fragment validation failed?",
-                    pathName
+                throw new InvalidOperationException(
+                    $"OpenAPI extension fragment tried to add a second path '$.paths.{pathName}', which is not supported. Extension fragment validation failed?"
                 );
             }
-            else
-            {
-                locationForPaths.Add(pathName, pathObject.DeepClone());
-            }
+
+            locationForPaths.Add(pathName, pathObject.DeepClone());
         }
     }
 
@@ -110,15 +153,12 @@ public class OpenApiDocument(ILogger _logger)
             // If schemaName has already been added by another extension, we don't support a second one
             if (locationForSchemas[schemaName] != null)
             {
-                _logger.LogError(
-                    "OpenAPI extension fragment tried to add a second schema '$.components.schemas.{SchemaName}', which is not supported. Extension fragment validation failed?",
-                    schemaName
+                throw new InvalidOperationException(
+                    $"OpenAPI extension fragment tried to add a second schema '$.components.schemas.{schemaName}', which is not supported. Extension fragment validation failed?"
                 );
             }
-            else
-            {
-                locationForSchemas.Add(schemaName, schemaObject.DeepClone());
-            }
+
+            locationForSchemas.Add(schemaName, schemaObject.DeepClone());
         }
     }
 
@@ -168,15 +208,12 @@ public class OpenApiDocument(ILogger _logger)
             // If tag has already been added by another extension, we don't support a second one
             if (existingTagNames.Contains(tagObjectName))
             {
-                _logger.LogError(
-                    "OpenAPI extension fragment tried to add a second tag named - {TagObjectName}, which is not supported. Extension fragment validation failed?",
-                    tagObjectName
+                throw new InvalidOperationException(
+                    $"OpenAPI extension fragment tried to add a second tag named '{tagObjectName}', which is not supported. Extension fragment validation failed?"
                 );
             }
-            else
-            {
-                globalTags.Add(newTagObject.DeepClone());
-            }
+
+            globalTags.Add(newTagObject.DeepClone());
         }
     }
 
@@ -325,11 +362,25 @@ public class OpenApiDocument(ILogger _logger)
                 documentSection.ToString()
             );
 
+            string? projectName = extensionApiSchemaRootNode
+                .SelectNodeFromPath("$.projectSchema.projectName", _logger)
+                ?.GetValue<string>()
+                .ToLower();
+
+            if (projectName != null)
+            {
+                _logger.LogInformation(
+                    "OpenAPI extension fragment found for project '{ProjectName}'",
+                    projectName
+                );
+            }
+
             foreach (JsonNode openApiExtensionFragment in openApiExtensionFragments)
             {
                 InsertExts(
                     openApiExtensionFragment.SelectRequiredNodeFromPath("$.exts", _logger).AsObject(),
-                    openApiSpecification
+                    openApiSpecification,
+                    projectName
                 );
 
                 InsertNewPaths(
