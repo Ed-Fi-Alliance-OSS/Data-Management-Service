a49c933710940dc65b97518aa32010973671644d
CSR2017 <suganya@imanisoftware.com>
2025-05-09 16:24:22 -0500

    Initial changes for Relationship with student only

--- src/dms/core/EdFi.DataManagementService.Core/Security/AuthorizationFilters/RelationshipsWithEdOrgsOnlyFiltersProvider.cs
+++ src/dms/core/EdFi.DataManagementService.Core/Security/AuthorizationFilters/RelationshipsWithEdOrgsOnlyFiltersProvider.cs
@@ -3,7 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using EdFi.DataManagementService.Core.External.Backend;
 using EdFi.DataManagementService.Core.External.Model;
 
 namespace EdFi.DataManagementService.Core.Security.AuthorizationFilters;
@@ -18,21 +17,6 @@ public class RelationshipsWithEdOrgsOnlyFiltersProvider : IAuthorizationFiltersP
 
     public AuthorizationStrategyEvaluator GetFilters(ClientAuthorizations authorizations)
     {
-        var filters = new List<AuthorizationFilter>();
-        var edOrgIdsFromClaim = authorizations
-            .EducationOrganizationIds.Select(e => e.Value.ToString())
-            .ToList();
-        if (edOrgIdsFromClaim.Count == 0)
-        {
-            string noRequiredClaimError =
-                $"The API client has been given permissions on a resource that uses the '{AuthorizationStrategyName}' authorization strategy but the client doesn't have any education organizations assigned.";
-            throw new AuthorizationException(noRequiredClaimError);
-        }
-        foreach (var edOrgId in edOrgIdsFromClaim)
-        {
-            filters.Add(new AuthorizationFilter(SecurityElementNameConstants.EducationOrganization, edOrgId));
-        }
-
-        return new AuthorizationStrategyEvaluator(AuthorizationStrategyName, [.. filters], FilterOperator.Or);
+        return RelationshipsFiltersProvider.GetFilters(authorizations, AuthorizationStrategyName);
     }
 }
