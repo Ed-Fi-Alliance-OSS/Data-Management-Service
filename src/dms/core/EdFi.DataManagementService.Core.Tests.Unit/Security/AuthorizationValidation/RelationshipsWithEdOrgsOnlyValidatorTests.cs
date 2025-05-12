682720088ee0f7c11db1a111f4405f3202ca40af
Axel Marquez <axel.marqz@gmail.com>
2025-05-12 11:19:55 -0600

    [DMS-621] Fix unit test

--- src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Security/AuthorizationValidation/RelationshipsWithEdOrgsOnlyValidatorTests.cs
+++ src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Security/AuthorizationValidation/RelationshipsWithEdOrgsOnlyValidatorTests.cs
@@ -262,6 +262,7 @@ public class RelationshipsWithEdOrgsOnlyValidatorTests
                     ),
                 ],
                 [],
+                [],
                 []
             );
         }
