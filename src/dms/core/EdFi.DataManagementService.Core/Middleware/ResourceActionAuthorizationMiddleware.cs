b0cf23fe836b5b5ad3f872b724e9d34c4eec90ba
CSR2017 <suganya@imanisoftware.com>
2025-06-03 18:01:57 -0500

    Remove disablePersonAuthorizationStrategies

--- src/dms/core/EdFi.DataManagementService.Core/Middleware/ResourceActionAuthorizationMiddleware.cs
+++ src/dms/core/EdFi.DataManagementService.Core/Middleware/ResourceActionAuthorizationMiddleware.cs
@@ -20,8 +20,7 @@ namespace EdFi.DataManagementService.Core.Middleware;
 /// </summary>
 internal class ResourceActionAuthorizationMiddleware(
     IClaimSetCacheService _claimSetCacheService,
-    ILogger _logger,
-    bool disablePersonAuthorizationStrategies = false
+    ILogger _logger
 ) : IPipelineStep
 {
     public async Task Execute(PipelineContext context, Func<Task> next)
@@ -121,13 +120,7 @@ internal class ResourceActionAuthorizationMiddleware(
 
             IReadOnlyList<string> resourceActionAuthStrategies = authorizedAction
                 .AuthorizationStrategies.Select(auth => auth.Name)
-                .Select(authStrategyName =>
-                    disablePersonAuthorizationStrategies
-                    && authStrategyName.StartsWith("RelationshipsWith")
-                    && authStrategyName != "RelationshipsWithEdOrgsOnly"
-                        ? "NoFurtherAuthorizationRequired"
-                        : authStrategyName
-                )
+                .Select(authStrategyName => authStrategyName)
                 .ToList();
 
             if (resourceActionAuthStrategies.Count == 0)
