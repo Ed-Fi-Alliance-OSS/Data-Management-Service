aae489b9226d973862f200d14c5573c84b4f0239
CSR2017 <34072978+CSR2017@users.noreply.github.com>
2025-06-23 11:56:31 -0500

    Fix the NEED_DATABASE_SETUP error (#619)

--- eng/docker-compose/start-local-dms.ps1
+++ eng/docker-compose/start-local-dms.ps1
@@ -108,9 +108,13 @@ else {
     # Create client with edfi_admin_api/authMetadata_readonly_access scope
     ./setup-keycloak.ps1 -NewClientId "CMSAuthMetadataReadOnlyAccess" -NewClientName "CMS Auth Endpoints Only Access" -ClientScopeName "edfi_admin_api/authMetadata_readonly_access"
 
+    Import-Module ./env-utility.psm1
+    $envValues = ReadValuesFromEnvFile $EnvironmentFile
+
     Write-Output "Starting locally-built DMS"
     $env:NEED_DATABASE_SETUP = if ($LoadSeedData) { "false" } else { $env:NEED_DATABASE_SETUP }
     docker compose $files --env-file $EnvironmentFile -p dms-local up $upArgs
+    $env:NEED_DATABASE_SETUP = $envValues["NEED_DATABASE_SETUP"]
 
     if ($LASTEXITCODE -ne 0) {
         throw "Unable to start local Docker environment, with exit code $LASTEXITCODE."
