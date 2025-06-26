a22750fa2676c5ff106545490023d786f1e1e4ce
CSR2017 <suganya@imanisoftware.com>
2025-06-26 12:55:24 -0500

    Automate

--- eng/docker-compose/start-local-dms.ps1
+++ eng/docker-compose/start-local-dms.ps1
@@ -53,7 +53,39 @@ if($LoadExtensionResourceClaims)
     Push-Location ../CmsHierarchy/
     Write-Output "Loading extension resource claims..."
     dotnet build CmsHierarchy.csproj -c Release
-    dotnet run --configuration Release --project "CmsHierarchy.csproj" --no-launch-profile -- --command TransformExtensionResourceClaims --input SampleExtensionResourceClaims.json  --outputFormat ToFile --output SecurityMetadata.json --outputFormat ToFile --skipAuths "RelationshipsWithEdOrgsAndPeopleInverted;RelationshipsWithStudentsOnlyThroughResponsibility;RelationshipsWithStudentsOnlyThroughResponsibilityIncludingDeletes"
+
+    dotnet run --configuration Release --project "CmsHierarchy.csproj" --no-launch-profile -- --command Transform --input "E2E-NameSpaceBasedClaimSet.json;E2E-NoFurtherAuthRequiredClaimSet.json;E2E-RelationshipsWithEdOrgsOnlyClaimSet.json;SampleExtensionResourceClaims.json"  --outputFormat ToFile --output ../docker-compose/SecurityMetadata.json --skipAuths "RelationshipsWithEdOrgsAndPeopleInverted;RelationshipsWithStudentsOnlyThroughResponsibility;RelationshipsWithStudentsOnlyThroughResponsibilityIncludingDeletes"
+    Pop-Location
+
+    $updatedAuthorizationHierarchyFilePath = "SecurityMetadata.json"
+
+    # Read JSON content
+    $jsonContent = Get-Content -Raw -Path $updatedAuthorizationHierarchyFilePath
+    $escapedJson = $jsonContent -replace "'", "''"
+
+    $existingScriptFilePath = Join-Path -Path $PSScriptRoot -ChildPath "..\..\src\config\backend\EdFi.DmsConfigurationService.Backend.Postgresql\Deploy\Scripts\0011_Insert_ClaimsHierarchy.sql"
+    $resolvedPath = Resolve-Path $existingScriptFilePath
+
+    # Read original .sql file content
+    $originalLines = Get-Content -Raw -Path $resolvedPath
+
+    # Retain only the commented lines (lines starting with "--")
+    $commentLines = Get-Content -Path $resolvedPath | Where-Object {
+        $_.TrimStart().StartsWith("--")
+    }
+
+# Define the new INSERT SQL
+$insertStatement = @"
+INSERT INTO dmscs.claimshierarchy(
+     hierarchy)
+VALUES ('$escapedJson'::jsonb);
+"@.Trim()
+
+    # Combine comments and new insert
+    $finalContent = $commentLines + "" + $insertStatement
+
+    # Write the updated file
+    Set-Content -Path $resolvedPath -Value $finalContent -Encoding UTF8
     }
     catch {
         Write-Error "Failed to load extension resource claims: $_"
@@ -129,8 +161,6 @@ else {
 
     # Create client with edfi_admin_api/authMetadata_readonly_access scope
     ./setup-keycloak.ps1 -NewClientId "CMSAuthMetadataReadOnlyAccess" -NewClientName "CMS Auth Endpoints Only Access" -ClientScopeName "edfi_admin_api/authMetadata_readonly_access"
-
-
 
     Import-Module ./env-utility.psm1
     $envValues = ReadValuesFromEnvFile $EnvironmentFile
