add8837c88ad6af32fe340d577fa765a960ea96d
Johnny Brenes <johnny.brenes@simpat.tech>
2025-07-08 15:12:57 -0600

    Scripts made errors when DeployDatabaseOnStartup set to true on Local

--- src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/Deploy/Scripts/0018_Create_ContactStudentSchoolAuthorization_Triggers.sql
+++ src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/Deploy/Scripts/0018_Create_ContactStudentSchoolAuthorization_Triggers.sql
@@ -39,7 +39,7 @@ BEGIN
             SELECT jsonb_array_elements(ContactStudentSchoolAuthorizationEducationOrganizationIds) AS edOrgIds
             FROM dms.ContactStudentSchoolAuthorization
             WHERE ContactUniqueId = contact_id
-        )
+        ) as t
   );
 END;
 $$ LANGUAGE plpgsql;
