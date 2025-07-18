9ff63bd43e4124565fb3774a513c365363a0b506
Jesus Flores <jesus@simpat.tech>
2025-07-17 23:19:37 -0600

    Fix local config e2etest execution

--- src/config/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore/Modules/HealthModule.cs
+++ src/config/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore/Modules/HealthModule.cs
@@ -3,12 +3,14 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Globalization;
+
 namespace EdFi.DmsConfigurationService.Frontend.AspNetCore.Modules;
 
 public class HealthModule : IEndpointModule
 {
     public void MapEndpoints(IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/health", () => Results.Text(DateTime.Now.ToString()));
+        endpoints.MapGet("/health", () => Results.Text(DateTime.Now.ToString(CultureInfo.InvariantCulture)));
     }
 }
