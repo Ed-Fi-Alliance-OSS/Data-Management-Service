cb75bdc8a53ef0f56ad63bc816ddd00ee8e0df6b
Jesus Flores <jesus@simpat.tech>
2025-07-17 23:36:57 -0600

    Load RoleClaimtype for dms

--- src/dms/frontend/EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit/TestAuthHandler.cs
+++ src/dms/frontend/EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit/TestAuthHandler.cs
@@ -5,6 +5,7 @@
 
 using System.Security.Claims;
 using System.Text.Encodings.Web;
+using EdFi.DataManagementService.Frontend.AspNetCore.Configuration;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 
@@ -13,7 +14,8 @@ namespace EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit;
 public class TestAuthHandler(
     IOptionsMonitor<AuthenticationSchemeOptions> options,
     Microsoft.Extensions.Logging.ILoggerFactory loggerFactory,
-    UrlEncoder encoder
+    UrlEncoder encoder,
+    IOptions<IdentitySettings> identitySettings
 ) : AuthenticationHandler<AuthenticationSchemeOptions>(options, loggerFactory, encoder)
 {
     protected override Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -21,7 +23,7 @@ public class TestAuthHandler(
         var claims = new[]
         {
             new Claim("client_id", AuthenticationConstants.ClientId),
-            new Claim(ClaimTypes.Role, AuthenticationConstants.Role),
+            new Claim(identitySettings.Value.RoleClaimType, identitySettings.Value.ClientRole),
         };
 
         var identity = new ClaimsIdentity(claims, AuthenticationConstants.AuthenticationSchema);
