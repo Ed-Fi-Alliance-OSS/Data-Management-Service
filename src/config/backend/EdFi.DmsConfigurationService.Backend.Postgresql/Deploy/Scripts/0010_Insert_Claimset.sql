a7aa67c395cad476d6bbc7d07cd9fdb467f6b826
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-07-23 15:40:43 -0600

    Update 0010_Insert_Claimset.sql
    Fix script to use the same order when inserting claimsets

--- src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0010_Insert_Claimset.sql
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0010_Insert_Claimset.sql
@@ -4,25 +4,32 @@
 -- See the LICENSE and NOTICES files in the project root for more information.
 
 INSERT INTO dmscs.ClaimSet (ClaimSetName, IsSystemReserved)
-SELECT v.ClaimSetName, v.IsSystemReserved FROM (
-    VALUES
-        ('E2E-NameSpaceBasedClaimSet', TRUE),
-        ('E2E-NoFurtherAuthRequiredClaimSet', TRUE),
-        ('E2E-RelationshipsWithEdOrgsOnlyClaimSet', TRUE),
-        ('SISVendor', TRUE),
-        ('EdFiSandbox', TRUE),
-        ('RosterVendor', TRUE),
-        ('AssessmentVendor', TRUE),
-        ('AssessmentRead', TRUE),
-        ('BootstrapDescriptorsandEdOrgs', TRUE),
-        ('DistrictHostedSISVendor', TRUE),
-        ('EdFiODSAdminApp', TRUE),
-        ('ABConnect', TRUE),
-        ('EdFiAPIPublisherReader', TRUE),
-        ('EdFiAPIPublisherWriter', TRUE),
-        ('FinanceVendor', TRUE),
-        ('EducationPreparationProgram', TRUE)
-) AS v(ClaimSetName, IsSystemReserved)
-WHERE NOT EXISTS (
-    SELECT 1 FROM dmscs.ClaimSet s WHERE s.ClaimSetName = v.ClaimSetName
-);
+SELECT v.ClaimSetName, v.IsSystemReserved
+FROM (
+    SELECT
+        ROW_NUMBER() OVER () AS OrderIndex,
+        ClaimSetName,
+        IsSystemReserved
+    FROM (
+        VALUES
+            ('E2E-NameSpaceBasedClaimSet', TRUE),
+            ('E2E-NoFurtherAuthRequiredClaimSet', TRUE),
+            ('E2E-RelationshipsWithEdOrgsOnlyClaimSet', TRUE),
+            ('SISVendor', TRUE),
+            ('EdFiSandbox', TRUE),
+            ('RosterVendor', TRUE),
+            ('AssessmentVendor', TRUE),
+            ('AssessmentRead', TRUE),
+            ('BootstrapDescriptorsandEdOrgs', TRUE),
+            ('DistrictHostedSISVendor', TRUE),
+            ('EdFiODSAdminApp', TRUE),
+            ('ABConnect', TRUE),
+            ('EdFiAPIPublisherReader', TRUE),
+            ('EdFiAPIPublisherWriter', TRUE),
+            ('FinanceVendor', TRUE),
+            ('EducationPreparationProgram', TRUE)
+    ) AS t(ClaimSetName, IsSystemReserved)
+) v
+LEFT JOIN dmscs.ClaimSet s ON s.ClaimSetName = v.ClaimSetName
+WHERE s.ClaimSetName IS NULL
+ORDER BY v.OrderIndex;
