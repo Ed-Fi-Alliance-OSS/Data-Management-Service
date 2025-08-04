f75e7c29b267a47135b486d3b9b6b88c5a22011a
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-08-04 10:08:20 -0600

    Add base components to include OIDC
    Add database components
    Add Openiddict components

---
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/OpenIddict/PostgresOidcServiceExtensions.cs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+using System.Data;
+using EdFi.DmsConfigurationService.Backend.OpenIddict;
+using EdFi.DmsConfigurationService.Backend.OpenIddict.Token;
+using EdFi.DmsConfigurationService.Backend.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace EdFi.DmsConfigurationService.Backend.Postgresql.OpenIddict
+{
+    public static class PostgresOidcServiceExtensions
+    {
+        public static IServiceCollection AddPostgresOpenIddictStores(this IServiceCollection services, string authority)
+        {
+            services.AddSingleton<IClientSqlProvider, PostgresClientSqlProvider>();
+            services.AddSingleton<IClientRepository, OpenIddictClientRepository>();
+            services.AddSingleton<ITokenManager, OpenIddictTokenManager>();
+            // services AddSingleton(typeof(IOpenIddictTokenStore<>), typeof(OpenIddictDapperTokenStore<>))
+            // TODO: services AddSingleton<ITokenManager, OpenIddictTokenManager>
+            // Register IDbConnection for Npgsql
+            services.AddTransient<IDbConnection>(sp =>
+             {
+                 var config = sp.GetRequiredService<IConfiguration>();
+                 return new NpgsqlConnection(config.GetConnectionString("DatabaseConnection"));
+             });
+            return services;
+        }
+    }
+}
