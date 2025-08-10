6ef9c289db696ec2253359bd1432c359defb6408
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-08-09 18:38:21 -0600

    Remove unused using
    Remove using

--- src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/OpenIddict/Repositories/PostgresOpenIddictClientRepository.cs
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/OpenIddict/Repositories/PostgresOpenIddictClientRepository.cs
@@ -3,15 +3,12 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Text.Json;
 using Dapper;
 using EdFi.DmsConfigurationService.Backend.Repositories;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Npgsql;
-using System.Text.Json;
 
 namespace EdFi.DmsConfigurationService.Backend.Postgresql.OpenIddict.Repositories
 {
