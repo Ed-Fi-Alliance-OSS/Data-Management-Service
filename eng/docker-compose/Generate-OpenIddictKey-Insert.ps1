3a6b337ffe4af6e178480c68d78a8adce97cb497
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-08-12 14:11:45 -0600

    Add test scripts
    Add instructiones to restclient test
    Add a script to create an insert statement of public/private key

---
+++ eng/docker-compose/Generate-OpenIddictKey-Insert.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+    Generates a 2048-bit RSA key pair and outputs a SQL insert statement for dmscs.OpenIddictKey.
+.DESCRIPTION
+    This script creates a new RSA key pair, encodes them in base64, and prints a SQL statement to insert them into the OpenIddictKey table, following the format in 0021_Insert_OpenIdKeys_Seed.sql.
+#>
+
+param(
+    [string]$KeyId = "key-$(Get-Random)",
+    [string]$EncryptionKey = "QWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo0NTY3ODkwMTIzNDU2Nzg5MDEyMw=="
+)
+
+$rsa = [System.Security.Cryptography.RSA]::Create(2048)
+$privateKey = $rsa.ExportPkcs8PrivateKey()
+$publicKey = $rsa.ExportSubjectPublicKeyInfo()
+
+$privateKeyBase64 = [Convert]::ToBase64String($privateKey)
+$publicKeyBase64 = [Convert]::ToBase64String($publicKey)
+
+$sql = @"
+INSERT INTO dmscs.OpenIddictKey (KeyId, PublicKey, PrivateKey, IsActive)
+VALUES ('$KeyId', decode('$publicKeyBase64', 'base64'), pgp_sym_encrypt('$privateKeyBase64', '$EncryptionKey'), TRUE);
+"@
+
+Write-Output $sql
