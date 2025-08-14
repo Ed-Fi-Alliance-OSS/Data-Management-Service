2d3c28800c67b8b205c8eea0a12faed072cc3fbc
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-08-14 15:57:22 -0600

    Update scripts
    Remove hardcoded encryption keys
    Update setup script to create public and private keys

--- src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0019_Create_OpenIdKeys_Table.sql
+++ src/config/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0019_Create_OpenIdKeys_Table.sql
@@ -3,8 +3,6 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 -- Create table for OpenID keys
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 CREATE TABLE IF NOT EXISTS dmscs.OpenIddictKey (
     Id SERIAL PRIMARY KEY,
     KeyId VARCHAR(64) NOT NULL,
