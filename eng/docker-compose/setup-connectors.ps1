abe0ddf8746b4931ce29920ffb9b1843da33eef8
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-09-19 11:33:12 -0600

    Update kafka containers
    Update kafka and kafka-ui to remove references to opensearch and elasticsearch

--- eng/docker-compose/setup-connectors.ps1
+++ eng/docker-compose/setup-connectors.ps1
@@ -3,12 +3,7 @@
 param (
     # Environment file
     [string]
-    $EnvironmentFile = "./.env",
-
-    # Search engine type ("OpenSearch" or "ElasticSearch")
-    [string]
-    [ValidateSet("OpenSearch", "ElasticSearch")]
-    $SearchEngine = "OpenSearch"
+    $EnvironmentFile = "./.env"
 )
 
 function IsReady([string] $Url) {
@@ -34,23 +29,9 @@ Import-Module ./env-utility.psm1
 $envFile = ReadValuesFromEnvFile $EnvironmentFile
 
 $sourcePort = $envFile["CONNECT_SOURCE_PORT"]
-$sinkPort = $envFile["CONNECT_SINK_PORT"]
 
 $sourceBase = "http://localhost:$sourcePort/connectors"
-$sinkBase = "http://localhost:$sinkPort/connectors"
 $sourceUrl = "$sourceBase/postgresql-source"
-$sinkUrl = "$sinkBase/opensearch-sink"
-
-$connectorConfig = "opensearch_connector.json"
-$AdminPassword = $envFile["OPENSEARCH_ADMIN_PASSWORD"]
-
-# Search engine check
-if($SearchEngine -eq "ElasticSearch")
-{
-    $sinkUrl = "$sinkBase/elasticsearch-sink"
-    $connectorConfig = "elasticsearch_connector.json"
-    $AdminPassword = $envFile["ELASTICSEARCH_ADMIN_PASSWORD"]
-}
 
 # Source connector
 if (IsReady($sourceBase)) {
@@ -84,33 +65,3 @@ else {
     Write-Output "Service at $sourceBase not available."
 }
 
-# Sink connector
-if (IsReady($sinkBase)) {
-    try {
-        $sinkResponse = Invoke-RestMethod -Uri $sinkUrl -Method Get -SkipHttpErrorCheck
-
-        if ($null -ne $sinkResponse.name) {
-            Write-Output "Deleting existing sink connector configuration."
-            Invoke-RestMethod -Method Delete -uri $sinkUrl
-        }
-    }
-    catch {
-        Write-Output $_.Exception.Message
-    }
-
-    try {
-        $sinkBody = Get-Content "./$connectorConfig"
-        $sinkBody = $sinkBody.Replace("abcdefgh1!", $AdminPassword)
-
-        Write-Output "Installing sink connector configuration"
-        Invoke-RestMethod -Method Post -uri $sinkBase -ContentType "application/json" -Body $sinkBody
-    }
-    catch {
-        Write-Output $_.Exception.Message
-    }
-    Start-Sleep 2
-    Invoke-RestMethod -Method Get -uri $sinkUrl -SkipHttpErrorCheck
-}
-else {
-    Write-Output "Service at $sinkBase not available."
-}
