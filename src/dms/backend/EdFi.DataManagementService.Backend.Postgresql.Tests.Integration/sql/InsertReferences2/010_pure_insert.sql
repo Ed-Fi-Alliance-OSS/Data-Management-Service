76ca8642e70b47ab342e011a1194741e2d155239
Brad Banister <brad@banisterconsult.com>
2025-11-20 08:42:56 -0600

    ok

---
+++ src/dms/backend/EdFi.DataManagementService.Backend.Postgresql.Tests.Integration/sql/InsertReferences2/010_pure_insert.sql
@@ -0,0 +1,16 @@
+-- Valid pure insert: expects success=true and empty invalid_ids.
+BEGIN;
+    SELECT * FROM dms.InsertReferences(
+        p_parentDocumentId        => (SELECT Id FROM dms.Document WHERE ResourceName = 'InsertReferences2Test' ORDER BY Id DESC LIMIT 1),
+        p_parentDocumentPartitionKey => 1::smallint,
+        p_referentialIds          => ARRAY[
+            '9a5226cd-6f14-c117-73b0-575f5505790c',
+            '0ae6e94d-d446-28f8-da03-240821ed958c',
+            'b9c20540-8759-0edc-feec-1c7775711621',
+            '0916dd27-b187-2c61-74c7-d88923aa800f',
+            'e3f62f16-1c78-4d6d-4da0-9f10457a7f7a'
+        ]::uuid[],
+        p_referentialPartitionKeys => ARRAY[12,12,1,15,10]::smallint[],
+        p_isPureInsert             => TRUE
+    );
+ROLLBACK;
