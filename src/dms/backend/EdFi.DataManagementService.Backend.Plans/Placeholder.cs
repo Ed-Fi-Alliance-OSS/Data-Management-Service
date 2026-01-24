c0f113b7d7d54089cf9f1c9ce2abef30b30529b9
Brad Banister <brad@banisterconsult.com>
2026-01-23 23:40:05 -0600

    [DMS-929] init

---
+++ src/dms/backend/EdFi.DataManagementService.Backend.Plans/Placeholder.cs
@@ -0,0 +1,3 @@
+namespace EdFi.DataManagementService.Backend.Plans;
+
+internal static class Placeholder;
---
+++ src/dms/backend/EdFi.DataManagementService.Backend.RelationalModel/Placeholder.cs
@@ -0,0 +1,3 @@
+namespace EdFi.DataManagementService.Backend.RelationalModel;
+
+internal static class Placeholder;
