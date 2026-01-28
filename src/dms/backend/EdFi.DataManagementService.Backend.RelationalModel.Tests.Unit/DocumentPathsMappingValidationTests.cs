2c5e460e8f13ab3ded44d0a81b75725f335291a2
Brad Banister <brad@banisterconsult.com>
2026-01-28 10:09:26 -0600

    Add abstract resource documentPathsMapping validation test

--- src/dms/backend/EdFi.DataManagementService.Backend.RelationalModel.Tests.Unit/DocumentPathsMappingValidationTests.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.RelationalModel.Tests.Unit/DocumentPathsMappingValidationTests.cs
@@ -117,3 +117,56 @@ public class Given_An_EffectiveSchemaSet_With_A_Missing_DocumentPathsMapping_Ref
         _exception.Message.Should().Contain("Ed-Fi:School");
     }
 }
+
+[TestFixture]
+public class Given_An_EffectiveSchemaSet_With_An_AbstractResource_Missing_DocumentPathsMapping_Target
+{
+    private Exception? _exception;
+
+    [SetUp]
+    public void Setup()
+    {
+        var projectSchema = EffectiveSchemaFixture.CreateProjectSchema();
+        EffectiveSchemaFixture.AddAbstractResources(projectSchema, ("abstractSchools", "AbstractSchool"));
+
+        var abstractResources = (JsonObject)projectSchema["abstractResources"]!;
+        var abstractSchoolSchema = (JsonObject)abstractResources["abstractSchools"]!;
+
+        abstractSchoolSchema["documentPathsMapping"] = new JsonObject
+        {
+            ["School"] = new JsonObject
+            {
+                ["isReference"] = true,
+                ["isDescriptor"] = false,
+                ["projectName"] = "Ed-Fi",
+                ["resourceName"] = "School",
+                ["path"] = "$.schoolReference",
+            },
+        };
+
+        var resourceKeys = new[] { EffectiveSchemaFixture.CreateResourceKey(1, "Ed-Fi", "AbstractSchool") };
+        var effectiveSchemaSet = EffectiveSchemaFixture.CreateEffectiveSchemaSet(projectSchema, resourceKeys);
+
+        var builder = new DerivedRelationalModelSetBuilder(Array.Empty<IRelationalModelSetPass>());
+
+        try
+        {
+            builder.Build(effectiveSchemaSet, SqlDialect.Pgsql, new PgsqlDialectRules());
+        }
+        catch (Exception ex)
+        {
+            _exception = ex;
+        }
+    }
+
+    [Test]
+    public void It_should_fail_with_a_missing_reference_target_in_abstract_resource()
+    {
+        _exception.Should().BeOfType<InvalidOperationException>();
+        _exception!.Message.Should().Contain("documentPathsMapping");
+        _exception.Message.Should().Contain("School");
+        _exception.Message.Should().Contain("$.schoolReference");
+        _exception.Message.Should().Contain("Ed-Fi:AbstractSchool");
+        _exception.Message.Should().Contain("Ed-Fi:School");
+    }
+}
