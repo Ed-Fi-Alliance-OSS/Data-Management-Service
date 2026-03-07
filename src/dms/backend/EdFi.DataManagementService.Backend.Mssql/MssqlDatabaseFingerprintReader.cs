153c8f1721c1ff9a9c5fe91e9a45ce6e31d6a4e1
Brad Banister <brad@banisterconsult.com>
2026-03-06 18:30:53 -0600

    Validate EffectiveSchema fingerprint contracts

--- src/dms/backend/EdFi.DataManagementService.Backend.Mssql/MssqlDatabaseFingerprintReader.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.Mssql/MssqlDatabaseFingerprintReader.cs
@@ -3,7 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Collections.Immutable;
+using EdFi.DataManagementService.Backend;
 using EdFi.DataManagementService.Core.External.Backend;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
@@ -13,42 +13,27 @@ namespace EdFi.DataManagementService.Backend.Mssql;
 public class MssqlDatabaseFingerprintReader(ILogger<MssqlDatabaseFingerprintReader> logger)
     : IDatabaseFingerprintReader
 {
-    public async Task<DatabaseFingerprint?> ReadFingerprintAsync(string connectionString)
-    {
-        await using var connection = new SqlConnection(connectionString);
-        await connection.OpenAsync();
+    private static readonly DatabaseFingerprintReaderQuery _query = new(
+        "dms.EffectiveSchema",
+        "SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dms' AND TABLE_NAME = 'EffectiveSchema'",
+        """
+        SELECT TOP (2) [EffectiveSchemaSingletonId], [ApiSchemaFormatVersion], [EffectiveSchemaHash], [ResourceKeyCount], [ResourceKeySeedHash]
+        FROM [dms].[EffectiveSchema]
+        ORDER BY [EffectiveSchemaSingletonId]
+        """,
+        new DatabaseFingerprintColumnNames(
+            EffectiveSchemaSingletonId: "EffectiveSchemaSingletonId",
+            ApiSchemaFormatVersion: "ApiSchemaFormatVersion",
+            EffectiveSchemaHash: "EffectiveSchemaHash",
+            ResourceKeyCount: "ResourceKeyCount",
+            ResourceKeySeedHash: "ResourceKeySeedHash"
+        )
+    );
 
-        // Check if the dms.EffectiveSchema table exists
-        await using var existsCommand = connection.CreateCommand();
-        existsCommand.CommandText =
-            "SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dms' AND TABLE_NAME = 'EffectiveSchema'";
-
-        if (await existsCommand.ExecuteScalarAsync() is null)
-        {
-            logger.LogDebug("dms.EffectiveSchema table does not exist");
-            return null;
-        }
-
-        // Read the singleton row
-        await using var readCommand = connection.CreateCommand();
-        readCommand.CommandText = """
-            SELECT [ApiSchemaFormatVersion], [EffectiveSchemaHash], [ResourceKeyCount], [ResourceKeySeedHash]
-            FROM [dms].[EffectiveSchema]
-            WHERE [EffectiveSchemaSingletonId] = 1
-            """;
-
-        await using var reader = await readCommand.ExecuteReaderAsync();
-        if (!await reader.ReadAsync())
-        {
-            logger.LogDebug("dms.EffectiveSchema table exists but has no singleton row");
-            return null;
-        }
-
-        return new DatabaseFingerprint(
-            ApiSchemaFormatVersion: reader.GetString(reader.GetOrdinal("ApiSchemaFormatVersion")),
-            EffectiveSchemaHash: reader.GetString(reader.GetOrdinal("EffectiveSchemaHash")),
-            ResourceKeyCount: reader.GetInt16(reader.GetOrdinal("ResourceKeyCount")),
-            ResourceKeySeedHash: ((byte[])reader[reader.GetOrdinal("ResourceKeySeedHash")]).ToImmutableArray()
+    public Task<DatabaseFingerprint?> ReadFingerprintAsync(string connectionString) =>
+        DatabaseFingerprintReaderSupport.ReadFingerprintAsync(
+            () => new SqlConnection(connectionString),
+            _query,
+            logger
         );
-    }
 }
--- src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/PostgresqlDatabaseFingerprintReader.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/PostgresqlDatabaseFingerprintReader.cs
@@ -3,7 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Collections.Immutable;
+using EdFi.DataManagementService.Backend;
 using EdFi.DataManagementService.Core.External.Backend;
 using Microsoft.Extensions.Logging;
 using Npgsql;
@@ -15,44 +15,28 @@ namespace EdFi.DataManagementService.Backend.Postgresql;
 public class PostgresqlDatabaseFingerprintReader(ILogger<PostgresqlDatabaseFingerprintReader> logger)
     : IDatabaseFingerprintReader
 {
-    public async Task<DatabaseFingerprint?> ReadFingerprintAsync(string connectionString)
-    {
-        await using var connection = new NpgsqlConnection(connectionString);
-        await connection.OpenAsync();
+    private static readonly DatabaseFingerprintReaderQuery _query = new(
+        "dms.EffectiveSchema",
+        "SELECT 1 FROM information_schema.tables WHERE table_schema = 'dms' AND table_name = 'effectiveschema'",
+        """
+        SELECT effectiveschemasingletonid, apischemaformatversion, effectiveschemahash, resourcekeycount, resourcekeyseedhash
+        FROM dms.effectiveschema
+        ORDER BY effectiveschemasingletonid
+        LIMIT 2
+        """,
+        new DatabaseFingerprintColumnNames(
+            EffectiveSchemaSingletonId: "effectiveschemasingletonid",
+            ApiSchemaFormatVersion: "apischemaformatversion",
+            EffectiveSchemaHash: "effectiveschemahash",
+            ResourceKeyCount: "resourcekeycount",
+            ResourceKeySeedHash: "resourcekeyseedhash"
+        )
+    );
 
-        // Check if the dms.effectiveschema table exists
-        // Note: PostgreSQL folds unquoted DDL identifiers to lowercase,
-        // so information_schema stores the table name as 'effectiveschema'.
-        await using var existsCommand = connection.CreateCommand();
-        existsCommand.CommandText =
-            "SELECT 1 FROM information_schema.tables WHERE table_schema = 'dms' AND table_name = 'effectiveschema'";
-
-        if (await existsCommand.ExecuteScalarAsync() is null)
-        {
-            logger.LogDebug("dms.effectiveschema table does not exist");
-            return null;
-        }
-
-        // Read the singleton row
-        await using var readCommand = connection.CreateCommand();
-        readCommand.CommandText = """
-            SELECT apischemaformatversion, effectiveschemahash, resourcekeycount, resourcekeyseedhash
-            FROM dms.effectiveschema
-            WHERE effectiveschemasingletonid = 1
-            """;
-
-        await using var reader = await readCommand.ExecuteReaderAsync();
-        if (!await reader.ReadAsync())
-        {
-            logger.LogDebug("dms.effectiveschema table exists but has no singleton row");
-            return null;
-        }
-
-        return new DatabaseFingerprint(
-            ApiSchemaFormatVersion: reader.GetString(reader.GetOrdinal("apischemaformatversion")),
-            EffectiveSchemaHash: reader.GetString(reader.GetOrdinal("effectiveschemahash")),
-            ResourceKeyCount: reader.GetInt16(reader.GetOrdinal("resourcekeycount")),
-            ResourceKeySeedHash: ((byte[])reader[reader.GetOrdinal("resourcekeyseedhash")]).ToImmutableArray()
+    public Task<DatabaseFingerprint?> ReadFingerprintAsync(string connectionString) =>
+        DatabaseFingerprintReaderSupport.ReadFingerprintAsync(
+            () => new NpgsqlConnection(connectionString),
+            _query,
+            logger
         );
-    }
 }
