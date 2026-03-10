8819ab4d79dfadede319c26620bf4e116be98641
Brad Banister <brad@banisterconsult.com>
2026-03-10 00:33:14 -0500

    Add token info pipeline ordering tests

--- src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Pipeline/PipelineOrderingTests.cs
+++ src/dms/core/EdFi.DataManagementService.Core.Tests.Unit/Pipeline/PipelineOrderingTests.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using EdFi.DataManagementService.Core.ApiSchema;
 using EdFi.DataManagementService.Core.Configuration;
 using EdFi.DataManagementService.Core.External.Backend;
+using EdFi.DataManagementService.Core.Handler;
 using EdFi.DataManagementService.Core.Middleware;
 using EdFi.DataManagementService.Core.Pipeline;
 using EdFi.DataManagementService.Core.Profile;
@@ -28,6 +29,25 @@ namespace EdFi.DataManagementService.Core.Tests.Unit.Pipeline;
 [Parallelizable]
 public class PipelineOrderingTests
 {
+    private static List<Type> GetStepTypes(ApiService apiService, string factoryMethodName)
+    {
+        var method = typeof(ApiService).GetMethod(
+            factoryMethodName,
+            BindingFlags.NonPublic | BindingFlags.Instance
+        );
+        method.Should().NotBeNull($"{factoryMethodName} should exist on ApiService");
+
+        var pipeline = (PipelineProvider)method!.Invoke(apiService, null)!;
+        var field = typeof(PipelineProvider)
+            .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+            .SingleOrDefault(info => info.FieldType == typeof(List<IPipelineStep>));
+
+        field.Should().NotBeNull("PipelineProvider should store its steps for execution");
+
+        var steps = (List<IPipelineStep>)field!.GetValue(pipeline)!;
+        return steps.Select(step => step.GetType()).ToList();
+    }
+
     [TestFixture]
     [Parallelizable]
     public class Given_The_Query_Pipeline : PipelineOrderingTests
@@ -99,21 +119,7 @@ public class PipelineOrderingTests
                 A.Fake<IProfileService>()
             );
 
-            var method = typeof(ApiService).GetMethod(
-                "CreateQueryPipeline",
-                BindingFlags.NonPublic | BindingFlags.Instance
-            );
-            method.Should().NotBeNull("CreateQueryPipeline should exist on ApiService");
-
-            var pipeline = (PipelineProvider)method!.Invoke(apiService, null)!;
-            var field = typeof(PipelineProvider)
-                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                .SingleOrDefault(info => info.FieldType == typeof(List<IPipelineStep>));
-
-            field.Should().NotBeNull("PipelineProvider should store its steps for execution");
-
-            var steps = (List<IPipelineStep>)field!.GetValue(pipeline)!;
-            _stepTypes = steps.Select(step => step.GetType()).ToList();
+            _stepTypes = GetStepTypes(apiService, "CreateQueryPipeline");
         }
 
         [Test]
@@ -165,6 +171,123 @@ public class PipelineOrderingTests
                 .BeLessThan(
                     apiSchemaValidationIndex,
                     "ValidateDatabaseFingerprintMiddleware must run before schema-dependent middleware"
+                );
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable]
+    public class Given_The_Get_Token_Info_Pipeline : PipelineOrderingTests
+    {
+        private List<Type> _stepTypes = [];
+
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+
+            services.Configure<JwtAuthenticationOptions>(options => { });
+            services.AddTransient<JwtAuthenticationMiddleware>();
+            services.AddTransient<IJwtValidationService>(_ => A.Fake<IJwtValidationService>());
+            services.AddTransient<ILogger<JwtAuthenticationMiddleware>>(_ =>
+                NullLogger<JwtAuthenticationMiddleware>.Instance
+            );
+
+            services.AddTransient<ResolveDmsInstanceMiddleware>();
+            services.AddSingleton<IApplicationContextProvider>(A.Fake<IApplicationContextProvider>());
+            services.AddSingleton<IDmsInstanceProvider>(A.Fake<IDmsInstanceProvider>());
+            services.AddSingleton<IDmsInstanceSelection>(A.Fake<IDmsInstanceSelection>());
+            services.AddTransient<ILogger<ResolveDmsInstanceMiddleware>>(_ =>
+                NullLogger<ResolveDmsInstanceMiddleware>.Instance
+            );
+
+            var appSettingsOptions = Options.Create(
+                new AppSettings { AllowIdentityUpdateOverrides = "", MaskRequestBodyInLogs = false }
+            );
+            services.AddSingleton(appSettingsOptions);
+            services.AddSingleton<IDatabaseFingerprintReader, NullDatabaseFingerprintReader>();
+            services.AddSingleton<DatabaseFingerprintProvider>();
+            services.AddTransient<ValidateDatabaseFingerprintMiddleware>();
+            services.AddTransient<ILogger<ValidateDatabaseFingerprintMiddleware>>(_ =>
+                NullLogger<ValidateDatabaseFingerprintMiddleware>.Instance
+            );
+
+            var claimSetProvider = A.Fake<IClaimSetProvider>();
+            var profileService = A.Fake<IProfileService>();
+
+            services.AddSingleton(claimSetProvider);
+            services.AddSingleton(profileService);
+            services.AddTransient<GetTokenInfoHandler>();
+            services.AddTransient<ILogger<GetTokenInfoHandler>>(_ =>
+                NullLogger<GetTokenInfoHandler>.Instance
+            );
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var apiService = new ApiService(
+                A.Fake<IApiSchemaProvider>(),
+                A.Fake<IEffectiveApiSchemaProvider>(),
+                claimSetProvider,
+                A.Fake<IDocumentValidator>(),
+                A.Fake<IMatchingDocumentUuidsValidator>(),
+                A.Fake<IEqualityConstraintValidator>(),
+                A.Fake<IDecimalValidator>(),
+                NullLogger<ApiService>.Instance,
+                appSettingsOptions,
+                A.Fake<IAuthorizationServiceFactory>(),
+                ResiliencePipeline.Empty,
+                A.Fake<ResourceLoadOrderCalculator>(),
+                serviceProvider,
+                A.Fake<IServiceScopeFactory>(),
+                A.Fake<CachedClaimSetProvider>(),
+                A.Fake<IResourceDependencyGraphMLFactory>(),
+                profileService
+            );
+
+            _stepTypes = GetStepTypes(apiService, "CreateGetTokenInfoPipeline");
+        }
+
+        [Test]
+        public void It_contains_ValidateDatabaseFingerprintMiddleware()
+        {
+            _stepTypes.Should().Contain(typeof(ValidateDatabaseFingerprintMiddleware));
+        }
+
+        [Test]
+        public void It_omits_ParsePathMiddleware()
+        {
+            _stepTypes.Should().NotContain(typeof(ParsePathMiddleware));
+        }
+
+        [Test]
+        public void It_places_fingerprint_validation_before_schema_dependent_middleware()
+        {
+            var fingerprintIndex = _stepTypes.IndexOf(typeof(ValidateDatabaseFingerprintMiddleware));
+            var apiSchemaValidationIndex = _stepTypes.IndexOf(typeof(ApiSchemaValidationMiddleware));
+
+            fingerprintIndex.Should().BeGreaterThanOrEqualTo(0);
+            apiSchemaValidationIndex.Should().BeGreaterThanOrEqualTo(0);
+            fingerprintIndex
+                .Should()
+                .BeLessThan(
+                    apiSchemaValidationIndex,
+                    "ValidateDatabaseFingerprintMiddleware must run before schema-dependent middleware"
+                );
+        }
+
+        [Test]
+        public void It_places_fingerprint_validation_before_GetTokenInfoHandler()
+        {
+            var fingerprintIndex = _stepTypes.IndexOf(typeof(ValidateDatabaseFingerprintMiddleware));
+            var handlerIndex = _stepTypes.IndexOf(typeof(GetTokenInfoHandler));
+
+            fingerprintIndex.Should().BeGreaterThanOrEqualTo(0);
+            handlerIndex.Should().BeGreaterThanOrEqualTo(0);
+            fingerprintIndex
+                .Should()
+                .BeLessThan(
+                    handlerIndex,
+                    "ValidateDatabaseFingerprintMiddleware must run before GetTokenInfoHandler"
                 );
         }
     }
