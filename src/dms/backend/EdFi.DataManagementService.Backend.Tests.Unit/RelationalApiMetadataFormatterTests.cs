adcd97e56186eabecdd8e0b8c9ea73fef8e074d1
Brad Banister <brad@banisterconsult.com>
2026-05-07 17:00:40 -0500

    Implement recursive ETag canonicalization

--- src/dms/backend/EdFi.DataManagementService.Backend.Tests.Unit/RelationalApiMetadataFormatterTests.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.Tests.Unit/RelationalApiMetadataFormatterTests.cs
@@ -54,18 +54,34 @@ public class Given_RelationalApiMetadataFormatter
         var document = JsonNode.Parse(
             """
             {
-              "name": "A&B <tag>",
               "id": "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb",
               "_etag": "stale",
-              "_lastModifiedDate": "2026-04-13T12:00:00Z"
+              "_lastModifiedDate": "2026-04-13T12:00:00Z",
+              "link": {
+                "rel": "self",
+                "href": "/ed-fi/schools/aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+              },
+              "name": "A&B <tag>",
+              "schoolReference": {
+                "schoolId": 255901,
+                "link": {
+                  "rel": "School",
+                  "href": "/ed-fi/schools/bbbbbbbb-1111-2222-3333-cccccccccccc"
+                }
+              }
             }
             """
         )!;
-        var canonicalDocument = document.DeepClone().AsObject();
-
-        canonicalDocument.Remove("_etag");
-        canonicalDocument.Remove("_lastModifiedDate");
-        canonicalDocument.Remove("id");
+        var canonicalDocument = JsonNode.Parse(
+            """
+            {
+              "name": "A&B <tag>",
+              "schoolReference": {
+                "schoolId": 255901
+              }
+            }
+            """
+        )!;
 
         var expectedHash = Convert.ToBase64String(
             SHA256.HashData(CanonicalJsonSerializer.SerializeToUtf8Bytes(canonicalDocument))
@@ -76,6 +92,178 @@ public class Given_RelationalApiMetadataFormatter
 
         RelationalApiMetadataFormatter.FormatEtag(document).Should().Be(expectedHash);
         RelationalApiMetadataFormatter.FormatEtag(document).Should().NotBe(legacySerializerHash);
+    }
+
+    [Test]
+    public void It_ignores_link_subtrees_in_root_reference_collection_nested_collection_and_extensions()
+    {
+        var documentWithLinks = JsonNode.Parse(
+            """
+            {
+              "id": "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb",
+              "_etag": "stale",
+              "_lastModifiedDate": "2026-04-13T12:00:00Z",
+              "link": {
+                "href": "/ed-fi/students/aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+              },
+              "studentUniqueId": "10001",
+              "schoolReference": {
+                "schoolId": 255901,
+                "link": {
+                  "href": "/ed-fi/schools/bbbbbbbb-1111-2222-3333-cccccccccccc"
+                }
+              },
+              "addresses": [
+                {
+                  "id": "collection-item-1",
+                  "streetNumberName": "100 Main St",
+                  "schoolReference": {
+                    "schoolId": 255901,
+                    "link": {
+                      "href": "/ed-fi/schools/bbbbbbbb-1111-2222-3333-cccccccccccc"
+                    }
+                  },
+                  "periods": [
+                    {
+                      "id": "nested-item-1",
+                      "beginDate": "2026-01-01",
+                      "schoolReference": {
+                        "schoolId": 255901,
+                        "link": {
+                          "href": "/ed-fi/schools/bbbbbbbb-1111-2222-3333-cccccccccccc"
+                        }
+                      }
+                    }
+                  ]
+                }
+              ],
+              "_ext": {
+                "twentyOne": {
+                  "cohorts": [
+                    {
+                      "educationOrganizationReference": {
+                        "educationOrganizationId": 255901,
+                        "link": {
+                          "href": "/ed-fi/educationOrganizations/bbbbbbbb-1111-2222-3333-cccccccccccc"
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+            """
+        )!;
+        var documentWithoutLinks = JsonNode.Parse(
+            """
+            {
+              "studentUniqueId": "10001",
+              "schoolReference": {
+                "schoolId": 255901
+              },
+              "addresses": [
+                {
+                  "streetNumberName": "100 Main St",
+                  "schoolReference": {
+                    "schoolId": 255901
+                  },
+                  "periods": [
+                    {
+                      "beginDate": "2026-01-01",
+                      "schoolReference": {
+                        "schoolId": 255901
+                      }
+                    }
+                  ]
+                }
+              ],
+              "_ext": {
+                "twentyOne": {
+                  "cohorts": [
+                    {
+                      "educationOrganizationReference": {
+                        "educationOrganizationId": 255901
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+            """
+        )!;
+
+        RelationalApiMetadataFormatter
+            .FormatEtag(documentWithLinks)
+            .Should()
+            .Be(RelationalApiMetadataFormatter.FormatEtag(documentWithoutLinks));
+    }
+
+    [Test]
+    public void It_changes_the_etag_when_a_real_reference_identity_value_changes()
+    {
+        var firstDocument = JsonNode.Parse(
+            """
+            {
+              "studentUniqueId": "10001",
+              "schoolReference": {
+                "schoolId": 255901
+              }
+            }
+            """
+        )!;
+        var secondDocument = JsonNode.Parse(
+            """
+            {
+              "studentUniqueId": "10001",
+              "schoolReference": {
+                "schoolId": 255902
+              }
+            }
+            """
+        )!;
+
+        RelationalApiMetadataFormatter
+            .FormatEtag(firstDocument)
+            .Should()
+            .NotBe(RelationalApiMetadataFormatter.FormatEtag(secondDocument));
+    }
+
+    [Test]
+    public void It_uses_the_same_canonicalization_path_for_descriptor_bodies()
+    {
+        var descriptorBody = new ExtractedDescriptorBody(
+            "uri://ed-fi.org/SchoolTypeDescriptor",
+            "Alternative",
+            "Alternative",
+            "Alternative school type",
+            new DateOnly(2025, 1, 15),
+            new DateOnly(2025, 12, 31),
+            "uri://ed-fi.org/SchoolTypeDescriptor#Alternative",
+            "SchoolTypeDescriptor"
+        );
+        var externalResponseDocument = JsonNode.Parse(
+            """
+            {
+              "id": "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb",
+              "_etag": "stale",
+              "_lastModifiedDate": "2026-04-13T12:00:00Z",
+              "link": {
+                "href": "/ed-fi/schoolTypeDescriptors/aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+              },
+              "namespace": "uri://ed-fi.org/SchoolTypeDescriptor",
+              "codeValue": "Alternative",
+              "shortDescription": "Alternative",
+              "description": "Alternative school type",
+              "effectiveBeginDate": "2025-01-15",
+              "effectiveEndDate": "2025-12-31"
+            }
+            """
+        )!;
+
+        RelationalApiMetadataFormatter
+            .FormatEtag(descriptorBody)
+            .Should()
+            .Be(RelationalApiMetadataFormatter.FormatEtag(externalResponseDocument));
     }
 
     [Test]
