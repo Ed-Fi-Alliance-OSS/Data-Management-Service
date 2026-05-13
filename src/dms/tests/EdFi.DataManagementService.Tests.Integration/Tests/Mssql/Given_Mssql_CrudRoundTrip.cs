2067a826af1950dc5ca5d8d8ddb265c242c9ab08
Adam Hopkins <adam.hopkins@simpat.tech>
2026-05-13 11:23:10 -0500

    [DMS-1022] Add SQL Server wrappers for new CRUD/query/reference scenarios

--- src/dms/tests/EdFi.DataManagementService.Tests.Integration/Tests/Mssql/Given_Mssql_CrudRoundTrip.cs
+++ src/dms/tests/EdFi.DataManagementService.Tests.Integration/Tests/Mssql/Given_Mssql_CrudRoundTrip.cs
@@ -16,4 +16,21 @@ public sealed class Given_Mssql_CrudRoundTrip : MssqlApiIntegrationTestBase
     [Test]
     public Task It_creates_and_reads_a_student() =>
         CrudRoundTripScenario.It_creates_and_reads_a_student(Harness);
+
+    [Test]
+    public Task It_updates_a_student_via_put() => CrudRoundTripScenario.It_updates_a_student_via_put(Harness);
+
+    [Test]
+    public Task It_deletes_a_student() => CrudRoundTripScenario.It_deletes_a_student(Harness);
+
+    [Test]
+    public Task It_pages_students_via_query() => CrudRoundTripScenario.It_pages_students_via_query(Harness);
+
+    [Test]
+    public Task It_rejects_create_with_missing_reference() =>
+        CrudRoundTripScenario.It_rejects_create_with_missing_reference(Harness);
+
+    [Test]
+    public Task It_rejects_delete_when_referenced() =>
+        CrudRoundTripScenario.It_rejects_delete_when_referenced(Harness);
 }
