f350b0676242e915f4043ab538c7b4b4affea937
Brad Banister <brad@banisterconsult.com>
2026-05-19 16:20:41 -0500

    fixup

--- src/dms/frontend/EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit/RelationalWriteSmokeTests.cs
+++ src/dms/frontend/EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit/RelationalWriteSmokeTests.cs
@@ -432,6 +432,7 @@ public class Given_A_Host_Using_The_Relational_Backend
                 services.AddSingleton(A.Fake<IRelationalReadMaterializer>());
                 services.AddSingleton(A.Fake<IReadableProfileProjector>());
                 services.AddSingleton(A.Fake<IReferenceResolver>());
+                services.AddSingleton(A.Fake<ISingleRecordRelationshipAuthorizationExecutor>());
                 services.AddSingleton(A.Fake<IDescriptorReadHandler>());
                 services.AddSingleton<IDescriptorWriteHandler>(new ThrowingDescriptorWriteHandler());
                 services.AddSingleton(A.Fake<IRelationalDeleteEtagPreconditionChecker>());
