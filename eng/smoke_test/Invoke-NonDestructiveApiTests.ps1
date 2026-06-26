217dbf34f38d56f79c547977af2ed29a97bdecc1
Johnny Brenes <johnny.brenes@simpat.tech>
2026-06-25 18:06:17 -0600

    rollback non destructive api test version

--- eng/smoke_test/Invoke-NonDestructiveApiTests.ps1
+++ eng/smoke_test/Invoke-NonDestructiveApiTests.ps1
@@ -28,7 +28,7 @@ $ErrorActionPreference = "Stop"
 Import-Module ../Package-Management.psm1 -Force
 Import-Module ./modules/SmokeTest.psm1
 
-$path = Get-SmokeTestTool -PackageVersion '7.3.20185'
+$path = Get-SmokeTestTool -PackageVersion '7.2.413'
 
 $parameters = @{
   BaseUrl = $BaseUrl
