304ff74fdc7aefb193c4a27dbd8bc9f6607c6f41
Brad Banister <brad@banisterconsult.com>
2026-06-29 16:03:29 -0500

    Deduplicate relational mapping set registrations

--- src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/PostgresqlServiceExtensions.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.Postgresql/PostgresqlServiceExtensions.cs
@@ -3,9 +3,8 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.DataManagementService.Backend;
 using EdFi.DataManagementService.Backend.External;
-using EdFi.DataManagementService.Backend.Plans;
-using EdFi.DataManagementService.Core.Startup;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -25,23 +24,7 @@ public static class PostgresqlServiceExtensions
         IConfiguration configuration
     )
     {
-        services.Configure<MappingSetProviderOptions>(configuration.GetSection("MappingPacks"));
-        services.TryAddSingleton<MappingSetCompiler>();
-        services.TryAddSingleton<IMappingPackStore, NoOpMappingPackStore>();
-        services.TryAddSingleton<IRuntimeMappingSetCompiler>(serviceProvider =>
-        {
-            var effectiveSchemaSetProvider =
-                serviceProvider.GetRequiredService<IEffectiveSchemaSetProvider>();
-            var mappingSetCompiler = serviceProvider.GetRequiredService<MappingSetCompiler>();
-
-            return new RuntimeMappingSetCompiler(
-                () => effectiveSchemaSetProvider.EffectiveSchemaSet,
-                mappingSetCompiler,
-                SqlDialect.Pgsql,
-                new PgsqlDialectRules()
-            );
-        });
-        services.TryAddSingleton<IMappingSetProvider, MappingSetProvider>();
+        services.AddRelationalMappingSetServices(configuration, SqlDialect.Pgsql, new PgsqlDialectRules());
 
         services.TryAddSingleton<NpgsqlDataSourceCache>();
         services.TryAddScoped<NpgsqlDataSourceProvider>();
