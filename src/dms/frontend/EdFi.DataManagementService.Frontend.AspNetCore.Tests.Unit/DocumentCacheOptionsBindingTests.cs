cb7e28501e64fb5834f57f6441c66de93a12b92f
Brad Banister <brad@banisterconsult.com>
2026-07-29 20:27:25 -0500

    Add DocumentCache configuration options

---
+++ src/dms/frontend/EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit/DocumentCacheOptionsBindingTests.cs
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.DataManagementService.Core.Configuration;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace EdFi.DataManagementService.Frontend.AspNetCore.Tests.Unit;
+
+[TestFixture]
+[Parallelizable]
+public class DocumentCacheOptionsBindingTests
+{
+    [Test]
+    public void It_binds_DocumentCacheOptions_from_the_supported_configuration_section()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                new Dictionary<string, string?>
+                {
+                    ["DataManagement:DocumentCache:Targets:0:TenantKey"] = "",
+                    ["DataManagement:DocumentCache:Targets:0:DataStoreId"] = "1",
+                    ["DataManagement:DocumentCache:ReadAcceleration:Enabled"] = "true",
+                    ["DataManagement:DocumentCache:ReadAcceleration:DirectFillTimeout"] = "00:00:00.125",
+                    ["DataManagement:DocumentCache:Projector:PollInterval"] = "00:00:07",
+                    ["DataManagement:DocumentCache:Projector:PageSize"] = "25",
+                    ["DataManagement:DocumentCache:Projector:MaxConcurrentTargets"] = "4",
+                    ["DataManagement:DocumentCache:Projector:FailureBackoff"] = "00:01:15",
+                    ["DataManagement:DocumentCache:Projector:BaselineHighWaterMark"] = "2500",
+                }
+            )
+            .Build();
+
+        DocumentCacheOptions options = new();
+        configuration.GetSection(DocumentCacheOptions.SectionName).Bind(options);
+
+        options.Targets.Should().ContainSingle();
+        options.Targets[0].TenantKey.Should().BeEmpty();
+        options.Targets[0].DataStoreId.Should().Be(1);
+        options.ReadAcceleration.Enabled.Should().BeTrue();
+        options.ReadAcceleration.DirectFillTimeout.Should().Be(TimeSpan.FromMilliseconds(125));
+        options.Projector.PollInterval.Should().Be(TimeSpan.FromSeconds(7));
+        options.Projector.PageSize.Should().Be(25);
+        options.Projector.MaxConcurrentTargets.Should().Be(4);
+        options.Projector.FailureBackoff.Should().Be(TimeSpan.FromSeconds(75));
+        options.Projector.BaselineHighWaterMark.Should().Be(2500);
+    }
+}
