176a6d904087efda85409a5328980f46fc77b1b0
Brad Banister <brad@banisterconsult.com>
2026-08-25 22:43:13 -0500

    DMS-1319 Move CDC provider setup mapper to production

--- src/dms/backend/EdFi.DataManagementService.Backend.Tests.Unit/CdcProviderSetupResultMapperTests.cs
+++ src/dms/backend/EdFi.DataManagementService.Backend.Tests.Unit/CdcProviderSetupResultMapperTests.cs
@@ -3,9 +3,9 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.DataManagementService.Backend.Cdc;
 using EdFi.DataManagementService.Backend.Ddl;
 using EdFi.DataManagementService.Backend.External;
-using EdFi.DataManagementService.Backend.Tests.Common;
 using FluentAssertions;
 using NUnit.Framework;
 using CoreCdc = EdFi.DataManagementService.Core.DocumentCache.Cdc;
